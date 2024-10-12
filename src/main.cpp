7af27dc5f0573884c92abba8773c525c62879f77
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-10-12 09:47:07 -0400

    learncpp.com: 12.9- Pointers and Const

--- src/main.cpp
+++ src/main.cpp
@@ -1,137 +1,95 @@
 #include <cstddef> // for NUL
 #include <iostream>
 
-// 12.8- Null Pointers
+// 12.9- Pointers and Const
 
 int main() {
 
-  // Besides a memory address, there is one additional value that a pointer can
-  // hold: a null value. A null value (often shortened to null) is a special
-  // value that means something has no value. When a pointer is holding a null
-  // value, it means the pointer is not pointing at anything. Such a pointer is
-  // called a null pointer.
-  // * BEST PRACTICE: Value initialize your pointers (to be null pointers) if
-  // you are not initializing them with the address of a valid object.
-  int *ptr{}; // ptr is now a null pointer, and is not holding an address
-
-  int x{5};
-  ptr = &x; // ptr now pointing at object x (no longer a null pointer)
-
-  std::cout << *ptr << '\n'; // print value of x through dereferenced ptr
-  ////////////////////////////
-  // Much like the keywords true and false represent Boolean literal values, the
-  // nullptr keyword represents a null pointer literal. We can use nullptr to
-  // explicitly initialize or assign a pointer a null value.
-
-  int value{5};
-  int *ptr2{&value}; // ptr2 is a valid pointer
-  ptr2 = nullptr;    // Can assign nullptr to make the pointer a null pointer
-
-  // someFunction(nullptr); // we can also pass nullptr to a function that has a
-  //  pointer parameter
-  /////////////////////////////
-  // Much like dereferencing a dangling (or wild) pointer leads to undefined
-  // behavior, dereferencing a null pointer also leads to undefined behavior. In
-  // most cases, it will crash your application
-  ptr = nullptr;
-  // std::cout << *ptr << '\n'; // Dereference the null pointer
-  ////////////////////////////
-  // Checking for null pointers
-
-  // use a conditional to test whether a pointer has value nullptr or not
-
-  // int x{5};
-  x = 5;
-  // int *ptr{&x};
-  ptr = &x;
-
-  if (ptr == nullptr) // explicit test for equivalence
-    std::cout << "ptr is null\n";
-  else
-    std::cout << "ptr is non-null\n";
-
-  int *nullPtr{};
-  std::cout << "nullPtr is "
-            << (nullPtr == nullptr
-                    ? "null\n"
-                    : "non-null\n"); // explicit test for equivalence
+  // const int x{5}; // x is now const
+  //  int *ptr{&x};   // compile error: cannot convert from const int* to int*
 
   /////////////////////////////////
-  // pointers will also implicitly convert to Boolean values: a null pointer
-  // converts to Boolean value false, and a non-null pointer converts to Boolean
-  // value true. This allows us to skip explicitly testing for nullptr and just
-  // use the implicit conversion to Boolean to test whether a pointer is a null
-  // pointer.
-  // int x { 5 };
-  // int* ptr { &x };
+  // To declare a pointer to a const value, use the const keyword before the
+  // pointer’s data type
+  // const int x{5};
+  // const int *c_ptr{&x}; // okay: ptr is pointing to a "const int"
 
-  // pointers convert to Boolean false if they are null, and Boolean true if
-  // they are non-null
-  if (ptr) // implicit conversion to Boolean
-    std::cout << "ptr is non-null\n";
-  else
-    std::cout << "ptr is null\n";
+  //*c_ptr = 6; // not allowed: we can't change a const value
+  // * Because the data type being pointed to is const, the value being pointed
+  // to can’t be changed.
+  // * because a pointer to const is not const itself (it just points to a const
+  // value), we can change what the pointer is pointing at by assigning the
+  // pointer a new address:
+  // const int x{5};
+  // const int *cptr{&x}; // ptr points to const int x
 
-  // int *nullPtr{};
-  std::cout << "nullPtr is "
-            << (nullPtr ? "non-null\n"
-                        : "null\n"); // implicit conversion to Boolean
+  // const int y{6};
+  // cptr = &y; // okay: ptr now points at const int y
 
-  ///////////////////////
-  // avoid dereferencing a null pointer by using a conditional to ensure a
-  // pointer is non-null before trying to dereference it
-  if (ptr)                     // if ptr is not a null pointer
-    std::cout << *ptr << '\n'; // okay to dereference
-  else
-    // do something else that doesn't involve dereferencing ptr (print an error
-    // message, do nothing at all, etc...)
-    /////////////////////////////////
-    // * Legacy null pointer literals: 0 and NULL
-    // float *ptr{0}; // ptr is now a null pointer (for example only, don't do
-    // this)
+  ///////////////////////////////////////////
+  // * pointer to const can point to non-const variables too
+  // int x{5};           // non-const
+  // const int *ptr{&x}; // ptr points to a "const int"
 
-    // float *ptr2; // ptr2 is uninitialized
-    // ptr2 = 0;      // ptr2 is now a null pointer (for example only, don't do
-    // this)
+  //*ptr = 6; // not allowed: ptr points to a "const int" so we can't change the
+  // value through ptr
+  // x = 6; // allowed: the value is still non-const when accessed through
+  // non-const identifier x
 
-    // there is a preprocessor macro named NULL (defined in the <cstddef>
-    // header). This macro is inherited from C, where it is commonly used to
-    // indicate a null pointe
-    double *d_ptr{NULL}; // ptr is a null pointer
+  // #####################################################################
+  ////////////////////////////////////////////////////////
+  // A const pointer is a pointer whose address can not be changed after
+  // initialization
+  ///////////////////////////////////////////////////////////
+  int x{5};
+  // int *const ptr{&x}; // const after the asterisk means this is a const
+  // pointer
+  //  * Just like a normal const variable, a const pointer must be initialized
+  //  upon definition, and this value can’t be changed via assignment:
+  int y{6};
 
-  // double *ptr2; // ptr2 is uninitialized
-  ptr2 = NULL; // ptr2 is now a null pointer
-  // * Both 0 and NULL should be avoided in modern C++ (use nullptr instead)
+  // int *const ptr{&x};  // okay: the const pointer is initialized to the
+  // address of x
+  // ptr = &y; // error: once initialized, a const pointer can not be changed.
+  //*ptr = 6; // okay: the value being pointed to is non-const
 
-  ////////////////////////////////
-  // Favor references over pointers whenever possible
+  // #####################################################################
+  ////////////////////////////////////////////////////////
+  // * it is possible to declare a const pointer to a const value by using the
+  // const keyword both before the type and after the asterisk:
+  int value{5};
+  const int *const ptr{&value}; // a const pointer to a const value
+  // A const pointer to a const value can not have its address changed, nor can
+  // the value it is pointing to be changed through the pointer. It can only be
+  // dereferenced to get the value it is pointing at.
 
-  // * Pointers and references both give us the ability to access some other
-  // object indirectly.
+  // you only need to remember 4 rules, and they are pretty logical:
 
-  // * Pointers have the additional abilities of being able to change what
-  // they are pointing at, and to be pointed at null. However, these pointer
-  // abilities are also inherently dangerous: A null pointer runs the risk
-  // of being dereferenced, and the ability to change what a pointer is
-  // pointing at can make creating dangling pointers easier
-  // int *ptr{};
-  ptr = nullptr;
+  // * A non-const pointer can be assigned another address to change what it is
+  // pointing at.
+  // * A const pointer always points to the same address, and this address can
+  // not be changed.
+  // * A pointer to a non-const value can change the value it is pointing to.
+  // These can not point to a const value.
+  // * A pointer to a const value treats the value as const when accessed
+  // through the pointer, and thus can not change the value it is pointing to.
+  // These can be pointed to const or non-const l-values (but not r-values,
+  // which don’t have an address).
 
-  {
-    int x{5};
-    ptr = &x; // assign the pointer to an object that will be destroyed (not
-              // possible with a reference)
-  } // ptr is now dangling and pointing to invalid object
+  int v{5};
 
-  /*
-    if (ptr)             // condition evaluates to true because ptr is not
-    nullptr std::cout << *ptr; // undefined behavior
-  */
+  int *ptr0{&v}; // points to an "int" but is not const itself, so this is a
+                 // normal pointer.
+  const int *ptr1{&v}; // points to a "const int" but is not const itself, so
+                       // this is a pointer to a const value.
+  int *const ptr2{&v}; // points to an "int" and is const itself, so this is a
+                       // const pointer (to a non-const value).
+  const int *const ptr3{&v}; // points to a "const int" and is const itself, so
+                             // this is a const pointer to a const value.
 
-  // Since references can’t be bound to null, we don’t have to worry about null
-  // references. And because references must be bound to a valid object upon
-  // creation and then can not be reseated, dangling references are harder to
-  // create.
+  // if the const is on the left side of the *, the const belongs to the value
+  // if the const is on the right side of the *, the const belongs to the
+  // pointer
 
   return 0;
 }
