4bb60c630764f09fdac506635a18ab81c6ef23e2
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-10-13 10:08:15 -0400

    learncpp.com Section 12.15- std::optional

--- src/main.cpp
+++ src/main.cpp
@@ -1,247 +1,126 @@
+// learncpp.com Section 12.15- std::optional
+/*Third, we could abandon returning a single value and return two values
+instead: one (of type bool) that indicates whether the function succeeded, and
+the other (of the desired return type) that holds the actual return value (if
+the function succeeded) or an indeterminate value (if the function failed). This
+is probably the best option of the bunch.
 
+Prior to C++17, choosing this latter option required you to implement it
+yourself. And while C++ provides multiple ways to do so, any roll-your-own
+approach will inevitably lead to inconsistencies and errors.
+
+C++17 introduces std::optional, which is a class template type that implements
+an optional value. That is, a std::optional<T> can either have a value of type
+T, or not. We can use this to implement the third option above*/
 #include <iostream>
-#include <string>
-#include <string_view>
+#include <optional> // for std::optional (C++17)
 
-// learncpp.com: section 12.14- Type deduction with Pointers, References, and
-// const
+// #########################TOP###############################################
+// Our function now optionally returns an int value
+std::optional<int> doIntDivision(int x, int y) {
+  if (y == 0)
+    return {}; // or return std::nullopt
+  return x / y;
+}
+
+int main() {
+
+  std::optional<int> o1{5};            // initialize with a value
+  std::optional<int> o2{};             // initialize with no value
+  std::optional<int> o3{std::nullopt}; // initialize with no value
+  std::optional<int> result1{doIntDivision(20, 5)};
+  std::optional<int> result2{doIntDivision(5, 0)};
+
+  if (o1.has_value()) // call has_value() to check if o1 has a value
+                      //  if (o2) // use implicit conversion to bool to check if
+                      //  o2 has a value
+
+    if (result1) // if the function returned a value
+      std::cout << "Result 1: " << *result1 << '\n'; // get the value
+    else
+      std::cout << "Result 1: failed\n";
+
+  if (result2)
+    std::cout << "Result 2: " << *result2 << '\n';
+  else
+    std::cout << "Result 2: failed\n";
+
+  // Get values from std::optional
+  std::cout << *o1; // dereference to get value stored in o1 (undefined behavior
+                    // if o1 does not have a value)
+  // std::cout << o2.value(); // call value() to get value stored in o2 (throws
+  //  std::bad_optional_access exception if o2 does not
+  //  have a value)
+  std::cout << o3.value_or(42); // call value_or() to get value stored in o3 (or
+                                // value `42` if o3 doesn't have a value)
+
+  return 0;
+}
+// BEST PRACTICE: Return a std::optional (instead of a sentinel value) for
+// functions that may fail, unless your function needs to return additional
+// information about why it failed
+// #################################BOTTOM#######################################
+
+// #########################TOP###############################################
+/* //  std::optional as optional function paramenter
+void printIDNumber(std::optional<const int> id = std::nullopt) {
+  if (id)
+    std::cout << "Your ID number is " << *id << ".\n";
+  else
+    std::cout << "Your ID number is not known.\n";
+}
+
+int main() {
+  printIDNumber(); // we don't know the user's ID yet
+
+  int userid{34};
+  printIDNumber(userid); // we know the user's ID now
+
+  printIDNumber(62); // we can also pass an rvalue
+
+  return 0;
+}
+ */
+// #################################BOTTOM#######################################
+
+// BEST PRACTICE:Prefer std::optional for optional return types.
+// Prefer function overloading for optional function parameters (when possible).
+// Otherwise, use std::optional<T> for optional arguments when T would normally
+// be passed by value. Favor const T* when T is expensive to copy
 
 /*
-// ###############################top########################################
-int main() {
-  int a{5};
-  auto b{a}; // b deduced as an int
+//ADVANCED
+// Although std::optional doesn’t support references directly, you can use
+// std::reference_wrapper (which we cover in lesson 17.5 -- Arrays of references
+// via std::reference_wrapper) to mimic a reference. Let’s take a look at what
+// the above program looks like using a std::string id and
+// std::reference_wrapper:
+#include <functional>  // for std::reference_wrapper
+#include <iostream>
+#include <optional>
+#include <string>
 
-  return 0;
-}
-// #################################bottom######################################
- */
-// ###############################top########################################
-
-/* // Type deduction will drop references. Can reapply the defintion
-std::string &getRef(); // some function that returns a reference
-
-int main() {
-  auto ref{getRef()}; // type deduced as std::string (not std::string&)
-  auto &ref2{getRef()}; // std::string& (reference dropped, reference reapplied)
-
-  return 0;
-} */
-// #################################bottom######################################
-
-// ###############################top########################################
-/* // Top-level const
-const int x;    // this const applies to x, so it is top-level
-int *const ptr; // this const applies to ptr, so it is top-level
-
-// Low Level Consts
-const int &ref; // this const applies to the object being referenced, so it is
-                // low-level
-const int *ptr; // this const applies to the object being pointed to, so it is
-                // low-level
-
-// A reference to a const value is always a low-level const. A pointer can have
-// a top-level, low-level, or both kinds of const
-const int
-    *const ptr; // the left const is low-level, the right const is top-level */
-// #################################bottom######################################
-
-// ###############################top########################################
-
-/* const std::string &getConstRef(); // some function that returns a reference
-to const
-
-int main() {
-  auto ref1{getConstRef()}; // std::string (reference dropped, then top-level
-                            // const dropped from result)
-
-  return 0;
-}
-// since getConstRef() returns a const std::string&, the reference is dropped
-// first, leaving us with a const std::string. This const is now a top-level
-// const, so it is also dropped, leaving the deduced type as std::string.
- */
-// #################################bottom######################################
-// ###############################top########################################
-/* // reapply a reference and/or const:
-const std::string &
-getConstRef(); // some function that returns a const reference
-
-int main() {
-  auto ref1{
-      getConstRef()}; // std::string (reference and top-level const dropped)
-  const auto ref2{getConstRef()}; // const std::string (reference dropped, const
-                                  // dropped, const reapplied)
-
-  auto &ref3{getConstRef()}; // const std::string& (reference dropped and
-                             // reapplied, low-level const not dropped)
-  // Normally the reference would be dropped first, but
-  // since we’ve reapplied the reference, it is not dropped.
-  // That means the type is still const std::string&. And
-  // since this const is a low-level const, it is not
-  // dropped. Thus the deduced type is const std::string&.
-
-  // The ref4 case works similarly to ref3, except we’ve reapplied the const
-  // qualifier as well. Since the type is already deduced as a reference to
-  // const, us reapplying const here is redundant. That said, using const here
-  // makes it explicitly clear that our result will be const (whereas in the
-  // ref3 case, the constness of the result is implicit and not obvious).
-
-  const auto &ref4{getConstRef()}; // const std::string& (reference dropped and
-                                   // reapplied, low-level const not dropped)
-
-  return 0;
-}
-// BEST PRACTICE- If you want a const reference, reapply the const qualifier
-// even when it’s not strictly necessary, as it makes your intent clear and
-// helps prevent mistakes. */
-// #################################bottom######################################
-// ###############################top########################################
-
-/* // * Constexpr is not part of an expression’s type, so it is not deduced by
-auto
-// * When defining a constexpr reference to a const variable (e.g. constexpr
-// const int&), we need to apply both constexpr (which applies to the reference)
-// and const (which applies to the type being referenced).
-constexpr std::string_view hello{"Hello"}; // implicitly const
-
-constexpr const std::string_view &
-getConstRef() // function is constexpr, returns a const std::string_view&
+struct Employee
 {
-  return hello;
+    std::string name{}; // expensive to copy
+    int id;
+};
+
+void printEmployeeID(std::optional<std::reference_wrapper<Employee>>
+e=std::nullopt)
+{
+    if (e)
+        std::cout << "Your ID number is " << e->get().id << ".\n";
+    else
+        std::cout << "Your ID number is not known.\n";
 }
 
-int main() {
-  auto ref1{getConstRef()}; // std::string_view (reference dropped and top-level
-                            // const dropped)
-  constexpr auto ref2{
-      getConstRef()}; // constexpr const std::string_view (reference dropped and
-                      // top-level const dropped, constexpr applied, implicitly
-                      // const)
+int main()
+{
+    printEmployeeID(); // we don't know the Employee yet
 
-  auto &ref3{getConstRef()}; // const std::string_view& (reference reapplied,
-                             // low-level const not dropped)
+    Employee e { "James", 34 };
+    printEmployeeID(e); // we know the Employee's ID now
 
-  constexpr const auto &ref4{
-      getConstRef()}; // constexpr const std::string_view& (reference reapplied,
-                      // low-level const not dropped, constexpr applied)
-
-  return 0;
-}
- */
-//  #################################bottom######################################
-//  ###############################top########################################
-/* // Unlike references, type deduction does not drop pointers
-std::string *getPtr(); // some function that returns a pointer
-
-int main() {
-  auto ptr1{getPtr()}; // std::string*
-
-  return 0;
+    return 0;
 } */
-// #################################bottom######################################
-//  ###############################top########################################
-/* // We can also use an asterisk in conjunction with pointer type deduction
-// (auto*) to make it clearer that the deduced type is a pointer:
-std::string *getPtr(); // some function that returns a pointer
-
-int main() {
-  auto ptr1{getPtr()};  // std::string*
-  auto *ptr2{getPtr()}; // std::string*
-  auto ptr3{*getPtr()};  // std::string (because we dereferenced getPtr())
-  auto *ptr4{*getPtr()}; // does not compile (initializer not a pointer)
-
-  return 0;
-}*/
-// #################################bottom######################################
-//  ###############################top########################################
-/* // there are differences in how auto and auto* behave when we introduce const
-// into the equation.
-
-// we have both the const pointer and the pointer to const cases to think about,
-// and we also have auto vs auto*. Just like with references, only top-level
-// const is dropped during pointer type deduction.
-
-std::string *getPtr(); // some function that returns a pointer
-
-int main() {
-  const auto ptr1{getPtr()}; // std::string* const
-  auto const ptr2{getPtr()}; // std::string* const
-  // When we use either auto const or const auto, we’re saying, “make whatever
-  // the deduced type is const”. So in the case of ptr1 and ptr2, the deduced
-  // type is std::string*, and then const is applied, making the final type
-  // std::string* const. This is similar to how const int and int const mean the
-  // same thing.
-
-  const auto *ptr3{getPtr()}; // const std::string*
-  auto *const ptr4{getPtr()}; // std::string* const
-  // when we use auto*, the order of the const qualifier matters. A const on the
-  // left means “make the deduced pointer type a pointer to const”, whereas a
-  // const on the right means “make the deduced pointer type a const pointer”.
-  // Thus ptr3 ends up as a pointer to const, and ptr4 ends up as a const
-  // pointer.
-
-  return 0;
-} */
-// #################################bottom######################################
-//  ###############################top########################################
-// example where the initializer is a const pointer to const
-int main() {
-  std::string s{};
-  const std::string *const ptr{&s};
-
-  auto ptr1{ptr};  // const std::string*
-  auto *ptr2{ptr}; // const std::string*
-  // The top-level const (the const on the pointer itself) is dropped. The
-  // low-level const on the object being pointed to is not dropped. So in both
-  // cases, the final type is const std::string*.
-
-  auto const ptr3{ptr}; // const std::string* const
-  const auto ptr4{ptr}; // const std::string* const
-  // he top-level const is dropped, but we’re reapplying it. The low-level const
-  // on the object being pointed to is not dropped. So in both cases, the final
-  // type is const std::string* const
-
-  auto *const ptr5{ptr}; // const std::string* const
-  const auto *ptr6{ptr}; // const std::string*
-  // In both cases, the top-level const is dropped. For ptr5, the auto* const
-  // reapplies the top-level const, so the final type is const std::string*
-  // const. For ptr6, the const auto* applies const to the type being pointed to
-  // (which in this case was already const), so the final type is const
-  // std::string*
-
-  const auto const ptr7{ptr};  // error: const qualifer can not be applied twice
-  const auto *const ptr8{ptr}; // const std::string* const
-  // In the ptr7 case, we’re applying the const qualifier twice, which is
-  // disallowed, and will cause a compile error.
-
-  // And finally, in the ptr8 case, we’re applying const on both sides of the
-  // pointer (which is allowed since auto* must be a pointer type), so the
-  // resulting types is const std::string* const.
-
-  return 0;
-}
-// BEST PRACTICE: If you want a const pointer, pointer to const, or const
-// pointer to const, reapply the const qualifier(s) even when it’s not
-// strictly necessary, as it makes your intent clear and helps prevent
-// mistakes.
-
-// TIP: Consider using auto* when deducing a pointer type. Using auto* in this
-// case makes it clearer that we are deducing a pointer type, enlists the
-// compiler’s help to ensure we don’t deduce a non-pointer type, and gives you
-// more control over const.
-
-// #################################bottom######################################
-
-// ###############################top########################################
-
-// #################################bottom######################################
-// ###############################top########################################
-
-// #################################bottom######################################
-// ###############################top########################################
-
-// #################################bottom######################################
-// ###############################top########################################
-
-// #################################bottom######################################
