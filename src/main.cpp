048749337e181d0a22b09906d0025b46476e3e53
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-10-13 18:20:07 -0400

    Learncpp.com: section 14.9- Introduction to Constructors

--- src/main.cpp
+++ src/main.cpp
@@ -2,127 +2,91 @@
 #include <string>
 #include <string_view>
 
-// Learncpp.com: section 14.8- Benefits of Data Hiding (encapsulation)
+// Learncpp.com: section 14.9- Introduction to Constructors
 
-// data hiding (also called information hiding or data abstraction) is a
-// technique used to enforce the separation of interface and implementation by
-// hiding (making inaccessible) the implementation of a program-defined data
-// type from users.
+// A constructor is a special member function that is automatically called after
+// a non-aggregate class type object is created.
 
-// * Data hiding make classes easier to use, and reduces complexity
+// Beyond determining how an object may be created, constructors generally
+// perform two functions:
 
-// * Data hiding allows us to maintain invariants (conditions that must be true
-// throughout the lifetime of an object in order for the object to stay in a
-// valid state)
+// * They typically perform initialization of any member variables (via a member
+// initialization list)
+// * They may perform other setup functions (via statements in the body of the
+// constructor). This might include things such as error checking the
+// initialization values, opening a file or database, etc…
 
-struct sEmployee // members are public by default
-{
-  std::string name{"John"};
+// After the constructor finishes executing, we say that the object has been
+// “constructed”, and the object should now be in a consistent, usable state.
 
-  // Our Employee struct has a class invariant that firstInitial should always
-  // equal the first character of name. If this is ever untrue, then the print()
-  // function will malfunction
-  char firstInitial{'J'}; // should match first initial of name
+// Note that aggregates are not allowed to have constructors -- so if you add a
+// constructor to an aggregate, it is no longer an aggregate.
 
-  void print() const {
-    std::cout << "Employee " << name << " has first initial " << firstInitial
-              << '\n';
-  }
-};
+// Unlike normal member functions, constructors have specific rules for how they
+// must be named:
 
-// Let’s rewrite this program, making our member variables private, and exposing
-// a member function to set the name of an Employee
+// * Constructors must have the same name as the class (with the same
+// capitalization).
+// - For template classes, this name excludes the template parameters.
+// * Constructors have no return type (not even void).
 
-class Employee // members are private by default
-{
-  std::string m_name{};
-  char m_firstInitial{};
+// Because constructors are typically part of the interface for your class, they
+// are usually public.
+
+#include <iostream>
+/*
+class Foo {
+private:
+  int m_x{};
+  int m_y{};
 
 public:
-  void setName(std::string_view name) {
-    m_name = name;
-    m_firstInitial =
-        name.front(); // use std::string::front() to get first letter of `name`
+  Foo(int x,
+      int y) // here's our constructor function that takes two initializers
+  {
+    std::cout << "Foo(" << x << ", " << y << ") constructed\n";
   }
 
-  void print() const {
-    std::cout << "Employee " << m_name << " has first initial "
-              << m_firstInitial << '\n';
-  }
+  void print() const { std::cout << "Foo(" << m_x << ", " << m_y << ")\n"; }
 };
 
 int main() {
-  sEmployee e{}; // defaults to "John" and 'J'
-  e.print();
-
-  e.name = "Mark"; // change employee's name to "Mark"
-  e.print();       // prints wrong initial
-
-  Employee new_e{};
-  new_e.setName("John");
-  new_e.print();
-
-  new_e.setName("Mark");
-  new_e.print();
+  Foo foo{6, 7}; // calls Foo(int, int) constructor
+  foo.print();
 
   return 0;
-}
+} */
 
-// * Data hiding allows us to do better error detection (and handling)
-// * Data hiding makes it possible to change implementation details without
-// breaking existing programs
-// * Classes with interfaces are easier to debug
+// * Constructor implicit conversion of arguments
+// - compiler will perform implicit conversion of arguments in a function call
+// (if needed) in order to match a function definition where the parameters are
+// a different type
+// * A constructor needs to be able to initialize the object being constructed
+// -- therefore, a constructor must not be const.
+// ######################################################
 
-// BEST PRACTICE: Prefer implementing functions as non-members when possible
-// (especially functions that contain application specific data or logic).
-// EXAMPLE:
-/*
-class Yogurt {
-  std::string m_flavor{"vanilla"};
+class Something {
+private:
+  int m_x{};
 
 public:
-  void setFlavor(std::string_view flavor) { m_flavor = flavor; }
+  Something() // constructors must be non-const
+  {
+    m_x = 5; // okay to modify members in non-const constructor
+  }
 
-  const std::string &getFlavor() const { return m_flavor; }
+  int getX() const { return m_x; } // const
 };
 
-// Best: non-member function print() is not part of the class interface
-void print(const Yogurt &y) {
-  std::cout << "The yogurt has flavor " << y.getFlavor() << '\n';
-}
-
 int main() {
-  Yogurt y{};
-  y.setFlavor("cherry");
-  print(y);
+
+  // Normally a non-const member function can’t be invoked on a const object.
+  // However, because the constructor is invoked implicitly, a non-const
+  // constructor can be invoked on a const object
+  const Something
+      s{}; // const object, implicitly invokes (non-const) constructor
+
+  std::cout << s.getX(); // prints 5
 
   return 0;
 }
- */
-
-/*
-When writing code outside of a class, we are required to declare variables and
-functions before we can use them. However, inside a class, this limitation does
-not exist. As noted in lesson 14.3 -- Member functions, we can order our members
-in any order we like.
-
-So how should we order them?
-
-There are two schools of thought here:
-
-* List your private members first, and then list your public member functions.
-This follows the traditional style of declare-before-use. Anybody looking at
-your class code will see how you’ve defined your data members before they are
-used, which can make reading through and understanding implementation details
-easier.
-* List your public members first, and put your private members down at the
-bottom. Because someone who uses your class is interested in the public
-interface, putting your public members first makes the information they need up
-top, and puts the implementation details (which are least important) last.
-
-In modern C++, the second method (public members go first) is more commonly
-recommended, especially for code that will be shared with other developers.*/
-
-// BEST PRACTICE- Declare public members first, protected members next, and
-// private members last. This spotlights the public interface and de-emphasizes
-// implementation details.
