4b161e18227590c3fd321ce4da7b60e0f07c3117
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-10-13 16:24:12 -0400

    Learncpp.com: section 13.8- Struct Aggregate Initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,100 @@
 #include <iostream>
+#include <string>
 
-// Learncpp.com: section 14.6- Class Access Functions
+// Learncpp.com: section 13.8- Struct Aggregate Initialization
 
-/*
-Access functions come in two flavors: getters and setters.
+// an aggregate data type (also called an aggregate) is any type that can
+// contain multiple data members. Some types of aggregates allow members to have
+// different types (e.g. structs), while others require that all members must be
+// of a single type
 
-Getters (also sometimes called accessors) are public member functions that
-return the value of a private member variable. Setters (also sometimes called
-mutators) are public member functions that set the value of a private member
-variable.
-
-Getters are usually made const, so they can be called on both const and
-non-const objects. Setters should be non-const, so they can modify the data
-members.
-*/
-
-class Date {
-private:
-  int m_year{2020};
-  int m_month{10};
-  int m_day{14};
-
-public:
-  void print() {
-    std::cout << m_year << '/' << m_month << '/' << m_day << '\n';
-  }
-
-  // Getters should provide “read-only” access to data. Therefore, the best
-  // practice is that they should return by either value (if making a copy of
-  // the member is inexpensive) or by const lvalue reference (if making a copy
-  // of the member is expensive)
-  int getYear() const { return m_year; }    // getter for year
-  void setYear(int year) { m_year = year; } // setter for year
-
-  int getMonth() const { return m_month; }      // getter for month
-  void setMonth(int month) { m_month = month; } // setter for month
-
-  int getDay() const { return m_day; }  // getter for day
-  void setDay(int day) { m_day = day; } // setter for day
+/* //  2 primary forms of aggregate initialization:
+struct Employee {
+  int id{};
+  int age{};
+  double wage{};
 };
 
-int main() {
-  Date d{};
-  d.setYear(2021);
-  std::cout << "The year is: " << d.getYear() << '\n';
-
-  return 0;
+// Overloading operator<< to print a struct
+std::ostream &operator<<(std::ostream &out, const Employee &e) {
+  out << e.id << ' ' << e.age << ' ' << e.wage;
+  return out;
 }
 
-/*
-There is a fair bit of discussion around cases in which access functions should
-be used or avoided. Many developers would argue that use of access functions
-violates good class design (a topic that could easily fill an entire book).
+int main() {
+  Employee frank = {1, 32,
+                    60000.0}; // copy-list initialization using braced list
+  Employee joe{2, 28,
+               45000.0}; // list initialization using braced list (preferred)
 
-For now, we’ll recommend a pragmatic approach. As you create your classes,
-consider the following:
+  // in C++20
+  // Employee robert(3, 45, 62500.0); // direct initialization using
+  // parenthesized list (C++20)
 
-* If your class has no invariants and requires a lot of access functions,
-consider using a struct (whose data members are public) and providing direct
-access to members instead.
-* Prefer implementing behaviors or actions instead of access functions. For
-example, instead of a setAlive(bool) setter, implement a kill() and a revive()
-function.
-* Only provide access functions in cases where the public would reasonably need
-to get or set the value of an individual member.
-*/
+  Employee joey{2, 28}; // joe.wage will be value-initialized to 0.0
+  std::cout << joe.wage << std::endl;
+  Employee joseph{}; // value-initialize all members
+
+  Employee jose{2, 28}; // jose.wage will be value-initialized to 0.0
+  std::cout << jose << '\n';
+
+  // struct type can be const (or constexpr)
+
+  const Employee Ann{}; // value-initialize all members
+  const Employee Linda{4, 21, 40000};
+
+  std::cout << Linda.wage << std::endl;
+
+  std::cout << joe << std::endl;
+  joe = {joe.id, 33, 66000.0}; // Joe had a birthday and got a raise
+  std::cout << joe << std::endl;
+  joe = {.id = joe.id,
+         .age = 34,
+         .wage = 90000.0}; // Joe had a birthday and got a raise
+  std::cout << joe << std::endl;
+
+  return 0;
+} */
+
+// When initializing a struct from a list of values, the initializers are
+// applied to the members in order of declaration
+struct Foo {
+  int a{};
+  int b{}; // just added
+  // Now all your initialization values have shifted, and worse, the compiler
+  // may not detect this as an error (after all, the syntax is still valid).
+  int c{};
+};
+
+// Overloading operator<< to print a struct
+std::ostream &operator<<(std::ostream &out, const Foo &e) {
+  out << e.a << ' ' << e.b << ' ' << e.c;
+  return out;
+}
+
+int main() {
+  // Foo f{1, 3}; // f.a = 1, f.c = 3
+
+  // Designated Initializers- allow to explicitly define which initilization
+  // values map to which members
+  // C++20 add a new way to initialize struct members
+  Foo f1{.a{1}, .c{3}}; // ok: f1.a = 1, f1.b = 0 (value initialized), f1.c = 3
+  Foo f2{.a = 1,
+         .c = 3}; // ok: f2.a = 1, f2.b = 0 (value initialized), f2.c = 3
+  // Foo f3{.b{2}, .a{1}}; // error: initialization order does not match order
+  // of declaration in struct
+
+  std::cout << f1 << std::endl;
+  std::cout << f2 << std::endl;
+
+  Foo foo{1, 2, 3};
+
+  Foo x = foo; // copy initialization
+  Foo y(foo);  // direct initialization
+  Foo z{foo};  // list initialization
+
+  std::cout << x << '\n';
+  std::cout << y << '\n';
+  std::cout << z << '\n';
+  return 0;
+}
