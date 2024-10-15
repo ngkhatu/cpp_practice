8b9004a5ad50c5a90cfeb4912a3e422d62ec70dd
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-10-15 16:21:46 -0400

    Learncpp.com: section 13.2- Unscoped enumerations

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,85 @@
-// Learncpp.com: section 13.1- Intro to program-defined (user-defined) types
+// Learncpp.com: section 13.2- Unscoped enumerations
+/*
+// Define a new unscoped enumeration named Color
+enum Color {
+  // Here are the enumerators
+  // These symbolic constants define all the possible values this type can hold
+  // Each enumerator is separated by a comma, not a semicolon
+  red,
+  green,
+  blue, // trailing comma optional but recommended
+}; // the enum definition must end with a semicolon
 
-// create entirely new, custom types for use in our programs
-
-// two different categories of compound types that can be used to create
-// program-defined types
-// - Enumerated types (including unscoped and scoped enumerations)
-// - Class types (including structs, classes, and unions).
-
-// * The definition for a program-defined type is called a type definition
-
-// Define a program-defined type named Fraction so the compiler understands what
-// a Fraction is (we'll explain what a struct is and how to use them later in
-// this chapter) This only defines what a Fraction type looks like, it doesn't
-// create one
-struct Fraction {
-  int numerator{};
-  int denominator{};
-};
-
-// * By convention, program-defined types are named starting with a capital
-// letter and don’t use a suffix (e.g. Fraction, not fraction, fraction_t, or
-// Fraction_t).
-
-// Now we can make use of our Fraction type
 int main() {
-  Fraction f{3, 4}; // this actually instantiates a Fraction object named f
+  // Define a few variables of enumerated type Color
+  Color apple{red};   // my apple is red
+  Color shirt{green}; // my shirt is green
+  Color cup{blue};    // my cup is blue
+
+  // Color socks{white}; // error: white is not an enumerator of Color
+  // Color hat{2};       // error: 2 is not an enumerator of Color
+
+  return 0;
+} */
+
+// Enumerators are implicitly constexpr.
+/*
+using Color = int; // define a type alias named Color
+
+// The following color values should be used for a Color
+constexpr Color red{ 0 };
+constexpr Color green{ 1 };
+constexpr Color blue{ 2 };
+
+int main()
+{
+    Color appleColor{ red };
+    Color shirtColor{ green };
+
+    return 0;
+}
+*/
+
+// Scoped Enum
+
+// BEST PRACTICE- Prefer putting your enumerations inside a named scope region
+// (such as a namespace or class) so the enumerators don’t pollute the global
+// namespace.
+
+#include <iostream>
+
+namespace Color {
+// The names Color, red, blue, and green are defined inside namespace Color
+enum Color {
+  red,
+  green,
+  blue,
+};
+} // namespace Color
+
+namespace Feeling {
+enum Feeling {
+  happy,
+  tired,
+  blue, // Feeling::blue doesn't collide with Color::blue
+};
+}
+
+int main() {
+  Color::Color paint{Color::blue};
+  Feeling::Feeling me{Feeling::blue};
+
+  switch (paint) {
+  case Color::red:
+    std::cout << "red" << std::endl;
+    break;
+  case Color::green:
+    std::cout << "green" << std::endl;
+    break;
+  case Color::blue:
+    std::cout << "blue" << std::endl;
+    break;
+  }
 
   return 0;
 }
-
-// BEST PRACTICE- Name your program-defined types starting with a capital letter
-// and do not use a suffix.
-
-// * To propagate type definitions into the code files that need them,
-// program-defined types are typically defined in header files, and then
-// #included into any code file that requires that type definition. These header
-// files are typically given the same name as the program-defined type (e.g. a
-// program-defined type named Fraction would be defined in Fraction.h)
