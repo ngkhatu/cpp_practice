0d02b1af57771cebc2bb7cfa055e1305d7a73d5f
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-10-16 06:37:48 -0400

    Learncpp.com: section 13.5- Introduction to Overloading the I/O Operator

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,74 @@
-// Learncpp.com: section 13.4- Converting an enumeration to and from a string
+// Learncpp.com: section 13.5- Introduction to Overloading the I/O Operator
 
-// Getting the name of an enumerator
+// Introduction to Operator Overloading
 
-// typical way to get the name of an enumerator is to write a function that
-// allows us to pass in an enumerator and returns the enumerator’s name as a
-// string. But that requires some way to determine which string should be
-// returned for a given enumerator
-// 1) use a switch statement
-// 2) use an array
+/*
+Basic operator overloading is fairly straightforward:
+
+Define a function using the name of the operator as the function’s name.
+Add a parameter of the appropriate type for each operand (in left-to-right
+order). One of these parameters must be a user-defined type (a class type or an
+enumerated type), otherwise the compiler will error. Set the return type to
+whatever type makes sense. Use a return statement to return the result of the
+operation.
+*/
+
+//  implement an overload of operator<< to print a Color
+/*
 #include <iostream>
-#include <optional> // for std::optional
+#include <string_view>
+
+// A std::ostream object already knows how to print a std::string_view using
+// operator<< (this comes as part of the standard library)
+enum Color {
+  black,
+  red,
+  blue,
+};
+
+constexpr std::string_view getColorName(Color color) {
+  switch (color) {
+  case black:
+    return "black";
+  case red:
+    return "red";
+  case blue:
+    return "blue";
+  default:
+    return "???";
+  }
+}
+
+// Teach operator<< how to print a Color
+// std::ostream is the type of std::cout, std::cerr, etc...
+// The return type and parameter type are references (to prevent copies from
+// being made)
+
+// A std::ostream object already knows how to print a std::string_view using
+// operator<< (this comes as part of the standard library)
+std::ostream &operator<<(std::ostream &out, Color color) {
+  out << getColorName(
+      color); // print our color's name to whatever output stream out
+  return out; // operator<< conventionally returns its left operand
+
+  // The above can be condensed to the following single line:
+  // return out << getColorName(color)
+}
+
+int main() {
+  Color shirt{blue};
+  std::cout << "Your shirt is " << shirt << '\n'; // it works!
+
+  return 0;
+}
+ */
+
+// A std::ostream object already knows how to print a std::string_view using
+// operator<< (this comes as part of the standard library)
+
+#include <iostream>
+#include <limits>
+#include <optional>
 #include <string>
 #include <string_view>
 
@@ -48,17 +107,41 @@ constexpr std::optional<Pet> getPetFromString(std::string_view sv) {
   return {};
 }
 
+// pet is an in/out parameter
+std::istream &operator>>(std::istream &in, Pet &pet) {
+  std::string s{};
+  in >> s; // get input string from user
+
+  std::optional<Pet> match{getPetFromString(s)};
+  if (match) // if we found a match
+  {
+    pet = *match; // dereference std::optional to get matching enumerator
+    return in;
+  }
+
+  // We didn't find a match, so input must have been invalid
+  // so we will set input stream to fail state
+  in.setstate(std::ios_base::failbit);
+
+  // On an extraction failure, operator>> zero-initializes fundamental types
+  // Uncomment the following line to make this operator do the same thing
+  // pet = {};
+
+  return in;
+}
+
 int main() {
   std::cout << "Enter a pet: cat, dog, pig, or whale: ";
-  std::string s{};
-  std::cin >> s;
+  Pet pet{};
+  std::cin >> pet;
 
-  std::optional<Pet> pet{getPetFromString(s)};
-
-  if (!pet)
-    std::cout << "You entered an invalid pet\n";
-  else
-    std::cout << "You entered: " << getPetName(*pet) << '\n';
+  if (std::cin) // if we found a match
+    std::cout << "You chose: " << getPetName(pet) << '\n';
+  else {
+    std::cin.clear(); // reset the input stream to good
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Your pet was not valid\n";
+  }
 
   return 0;
 }
