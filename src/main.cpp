dddf1386c7d7b7f136599a3fbbfce0639808ee04
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-10-17 16:28:19 -0400

    Learncpp.com: section 7.10 (static local variables)

--- src/main.cpp
+++ src/main.cpp
@@ -1,110 +1,61 @@
-// Learncpp.com: section 7.9- Sharing global constants across multiple
-// files(using inline variables)
+// Learncpp.com: section 7.10 (static local variables)
 
-// In some applications, certain symbolic constants may need to be used
-// throughout your code (not just in one location). These can include physics or
-// mathematical constants that don’t change (e.g. pi or Avogadro’s number), or
-// application-specific “tuning” values (e.g. friction or gravity coefficients).
-// Instead of redefining these constants in every file that needs them (a
-// violation of the “Don’t Repeat Yourself” rule), it’s better to declare them
-// once in a central location and use them wherever needed. That way, if you
-// ever need to change them, you only need to change them in one place, and
-// those changes can be propagated out.
-
-// ################################################################
-//  METHOD 1
-//  Prior to C++17. add constants in a header file (constants.h)
-//  Downsides:
-//* Changing a single constant value would require recompiling every file that
-//  includes the constants header, which can lead to lengthy rebuild times for
-//  larger projects.
-//  * If the constants are large in size and can’t be optimized away, this can
-//  use a lot of memory.
-/*
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-
-// define your own namespace to hold constants
-namespace constants
-{
-    // constants have internal linkage by default
-    constexpr double pi { 3.14159 };
-    constexpr double avogadro { 6.0221413e23 };
-    constexpr double myGravity { 9.2 }; // m/s^2 -- gravity is light on this
-planet
-    // ... other related constants
-}
-#endif
-*/
-
-// #####################################################
-/* // METHOD 2
-// One way to avoid these problems is by turning these constants into external
-// variables, since we can then have a single variable (initialized once) that
-// is shared across all files. In this method, we’ll define the constants in a
-// .cpp file (to ensure the definitions only exist in one place), and put
-// forward declarations in the header (which will be included by other files)
-#include "constants.h" // include all the forward declarations
+// Using the static keyword on a local variable changes its duration from
+// automatic duration to static duration. This means the variable is now created
+// at the start of the program, and destroyed at the end of the program (just
+// like a global variable). As a result, the static variable will retain its
+// value even after it goes out of scope!
 
 #include <iostream>
 
-int main() {
-  std::cout << "Enter a radius: ";
-  double radius{};
-  std::cin >> radius;
+// BEST PRACTICE- Initialize your static local variables. Static local variables
+// are only initialized the first time the code is executed, not on subsequent
+// calls.
 
-  std::cout << "The circumference is: " << 2 * radius * constants::pi << '\n';
+// BEST PRACTICE- Const static local variables are generally okay to use.
+// * Non-const static local variables should generally be avoided. If you do use
+// them, ensure the variable never needs to be reset, and isn’t used to alter
+// program flow.
+
+void incrementAndPrint() {
+
+  // Static local variables that have no initializer or a non-constexpr
+  // initializer are zero-initialized at program start. Static local variables
+  // with a non-constexpr initializer are reinitialized the first time the
+  // variable definition is encountered. The definition is skipped on subsequent
+  // calls, so no futher reinitialization happens. Because they have static
+  // duration, static local variables that are not explicitly initialized will
+  // be zero-initialized by default.
+  static int s_value{1}; // static duration via static keyword.  This
+                         // initializer is only executed once.
+  ++s_value;
+  std::cout << s_value << '\n';
+} // s_value is not destroyed here, but becomes inaccessible because it goes out
+  // of scope
+
+int main() {
+  incrementAndPrint();
+  incrementAndPrint();
+  incrementAndPrint();
 
   return 0;
 }
- */
 
+// #######################################################
+
+// Static local variables can be made const (or constexpr). One good use for a
+// const static local variable is when you have a function that needs to use a
+// const value, but creating or initializing the object is expensive (e.g. you
+// need to read the value from a database). If you used a normal local variable,
+// the variable would be created and initialized every time the function was
+// executed. With a const/constexpr static local variable, you can create and
+// initialize the expensive object once, and then reuse it whenever the function
+// is called.
+
+// Key insight- Static local variables are best used to avoid expensive local
+// object initialization each time a function is called.
+
+// * Static local variables should only be used if in your entire program and in
+// the foreseeable future of your program, the variable is unique and it
+// wouldn’t make sense to reset the variable.
 // #####################################################
-// METHOD 3
-// By making our constexpr variables inline, we can define them in a header file
-// and then #include them into any .cpp file that requires them. This avoids
-// both ODR violations and the downside of duplicated variables.
-// * Constexpr functions are implicitly inline, but constexpr variables are not
-// implicitly inline. If you want an inline constexpr variable, you must
-// explicitly mark it as inline.
-
-// * This method does retain the downside of requiring every file that includes
-// the constants header be recompiled if any constant value is changed.
-
-// BEST PRACTICE-If you need global constants and your compiler is C++17
-// capable, prefer defining inline constexpr global variables in a header file.
-
-// constants.h
-/* #ifndef CONSTANTS_H
-#define CONSTANTS_H
-
-// define your own namespace to hold constants
-namespace constants
-{
-    inline constexpr double pi { 3.14159 }; // note: now inline constexpr
-    inline constexpr double avogadro { 6.0221413e23 };
-    inline constexpr double myGravity { 9.2 }; // m/s^2 -- gravity is light on
-this planet
-    // ... other related constants
-}
-#endif
-*/
-
-//--------------------------------------------
-// main.cpp
-/*
-#include "constants.h"
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Enter a radius: ";
-    double radius{};
-    std::cin >> radius;
-
-    std::cout << "The circumference is: " << 2 * radius * constants::pi << '\n';
-
-    return 0;
-}
-*/
