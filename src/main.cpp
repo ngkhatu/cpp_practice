9114b91876a8c0df5ac74d80c1bdf84acedcd408
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-10-17 07:22:23 -0400

    Learncpp.com: section- 7.7 — External linkage and variable forward declarations

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,75 @@
-// Learncpp.com: section 8.15- Global Random Numbers (Random.h)
+// Learncpp.com: section- 7.7 — External linkage and variable forward
+// declarations
 
-/*
-What happens if we want to use a random number generator in multiple functions
-or files? One way is to create (and seed) our PRNG in our main() function, and
-then pass it everywhere we need it. But that’s a lot of passing for something we
-may only use sporadically, and in many different places. It would add a lot of
-clutter to our code to pass such an object around.
+// An identifier with external linkage can be seen and used both from the file
+// in which it is defined, and from other code files (via a forward
+// declaration). In this sense, identifiers with external linkage are truly
+// “global” in that they can be used anywhere in your program
 
-Alternately, you could create a static local std::mt19937 variable in each
-function that needs it (static so that it only gets seeded once). However, it’s
-overkill to have every function that uses a random number generator define and
-seed its own local generator, and the low volume of calls to each generator may
-lead to lower quality results.
+// * In order to call a function defined in another file, you must place a
+// forward declaration for the function in any other files wishing to use the
+// function. The forward declaration tells the compiler about the existence of
+// the function, and the linker connects the function calls to the actual
+// function definition.
 
-What we really want is a single PRNG object that we can share and access
-anywhere, across all of our functions and files. The best option here is to
-create a global random number generator object (inside a namespace!). Remember
-how we told you to avoid non-const global variables? This is an exception
-*/
-
-#include "Random.h" // defines Random::mt, Random::get(), and Random::generate()
-#include <cstddef>  // for std::size_t
 #include <iostream>
 
+extern int g_x; // this extern is a forward declaration of a variable named g_x
+                // that is defined somewhere else
+extern const int g_y; // this extern is a forward declaration of a const
+                      // variable named g_y that is defined somewhere else
+
+// Note that function forward declarations don’t need the extern keyword -- the
+// compiler is able to tell whether you’re defining a new function or making a
+// forward declaration based on whether you supply a function body or not.
+// Variables forward declarations do need the extern keyword to help
+// differentiate uninitialized variables definitions from variable forward
+// declarations (they look otherwise identical):
+void sayHi(); // forward declaration for function sayHi, makes sayHi
+// accessible
+//  in this file
+
 int main() {
-  // We can use Random::get() to generate random numbers
+  sayHi(); // call to function defined in another file, linker will connect this
+  // call to the function definition
 
-  std::cout << Random::get(1, 6) << '\n'; // returns int between 1 and 6
-  std::cout << Random::get(1u, 6u)
-            << '\n'; // returns unsigned int between 1 and 6
-
-  // The following uses a template type argument
-  // See https://www.learncpp.com/cpp-tutorial/function-template-instantiation/
-  std::cout << Random::get<std::size_t>(1, 6u)
-            << '\n'; // returns std::size_t between 1 and 6
-
-  // We can access Random::mt directly if we have our own distribution
-
-  // Let's create a reusable random number generator that generates uniform
-  // numbers between 1 and 6
-  std::uniform_int_distribution die6{
-      1, 6}; // for C++14, use std::uniform_int_distribution<> die6{ 1, 6 };
-  for (int count{1}; count <= 10; ++count) {
-    std::cout << die6(Random::mt) << '\t'; // generate a roll of the die here
-  }
-
-  std::cout << '\n';
+  std::cout << g_x << ' ' << g_y << '\n'; // prints 2 3
 
   return 0;
 }
+
+// ###########################################
+// Global Variables with external linkage
+// use the 'extern' keyword
+
+int g_x{2}; // non-constant globals are external by default
+
+extern const int g_y{
+    3}; // const globals can be defined as extern, making them external
+extern constexpr int g_z{
+    3}; // constexpr globals can be defined as extern, making them external (but
+        // this is pretty useless, see the warning in the next section)
+//--------------------------------------------------
+
+// non-constant
+int g_x;        // variable definition (can have initializer if desired)
+extern int g_x; // forward declaration (no initializer)
+
+// constant
+extern const int g_y{1}; // variable definition (const requires initializers)
+extern const int g_y;    // forward declaration (no initializer)
+
+// SUMMARY
+//----------------------------------------------
+// Forward declarations:
+extern int g_y;                // forward declaration for non-constant global
+variable extern const int g_y; // forward declaration for const global
+variable extern constexpr int
+    g_y; // not allowed: constexpr variables can't be forward declared
+
+// External global variable definitions:
+int g_x; // defines non-initialized external global variable (zero initialized
+         // by default)
+extern const int g_x{1}; // defines initialized const external global variable
+extern constexpr int g_x{
+    2}; // defines initialized constexpr external global variable
