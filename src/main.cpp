ffe5cccc7e6fe8347f915abfecc5c836f0f88bfe
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-10-17 17:05:42 -0400

    Learncpp.com: section 9.4- Detecting and Handling Errors

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,59 @@
-// Learncpp.com: section 7.10 (static local variables)
+// Learncpp.com: section 9.4- Detecting and Handling Errors
 
-// Using the static keyword on a local variable changes its duration from
-// automatic duration to static duration. This means the variable is now created
-// at the start of the program, and destroyed at the end of the program (just
-// like a global variable). As a result, the static variable will retain its
-// value even after it goes out of scope!
+// most errors occur due to faulty assumptions made by the programmer and/or a
+// lack of proper error detection/handling.
+/*
+* When a function returns, the programmer may have assumed the called function
+was successful when it was not.
+* When a program receives input (either from the user, or a file), the
+programmer may have assumed the input was in the correct format and semantically
+valid when it was not.
+* When a function has been called, the programmer may have assumed the arguments
+would be semantically valid when they were not.
+*/
+
+// If the error is so bad that the program can not continue to operate properly,
+// this is called a non-recoverable error (also called a fatal error). In such
+// cases, the best thing to do is terminate the program. If your code is in
+// main() or a function called directly from main(), the best thing to do is let
+// main() return a non-zero status code. However, if you’re deep in some nested
+// subfunction, it may not be convenient or possible to propagate the error all
+// the way back to main(). In such a case, a halt statement (such as
+// std::exit()) can be used.
 
 #include <iostream>
 
-// BEST PRACTICE- Initialize your static local variables. Static local variables
-// are only initialized the first time the code is executed, not on subsequent
-// calls.
-
-// BEST PRACTICE- Const static local variables are generally okay to use.
-// * Non-const static local variables should generally be avoided. If you do use
-// them, ensure the variable never needs to be reset, and isn’t used to alter
-// program flow.
-
-void incrementAndPrint() {
-
-  // Static local variables that have no initializer or a non-constexpr
-  // initializer are zero-initialized at program start. Static local variables
-  // with a non-constexpr initializer are reinitialized the first time the
-  // variable definition is encountered. The definition is skipped on subsequent
-  // calls, so no futher reinitialization happens. Because they have static
-  // duration, static local variables that are not explicitly initialized will
-  // be zero-initialized by default.
-  static int s_value{1}; // static duration via static keyword.  This
-                         // initializer is only executed once.
-  ++s_value;
-  std::cout << s_value << '\n';
-} // s_value is not destroyed here, but becomes inaccessible because it goes out
-  // of scope
-
-int main() {
-  incrementAndPrint();
-  incrementAndPrint();
-  incrementAndPrint();
-
-  return 0;
+double doIntDivision(int x, int y) {
+  if (y == 0) {
+    std::cout << "Error: Could not divide by zero\n";
+    std::exit(1);
+  }
+  return x / y;
 }
 
-// #######################################################
+int main() {
 
-// Static local variables can be made const (or constexpr). One good use for a
-// const static local variable is when you have a function that needs to use a
-// const value, but creating or initializing the object is expensive (e.g. you
-// need to read the value from a database). If you used a normal local variable,
-// the variable would be created and initialized every time the function was
-// executed. With a const/constexpr static local variable, you can create and
-// initialize the expensive object once, and then reuse it whenever the function
-// is called.
+  double result = doIntDivision(5, 0);
 
-// Key insight- Static local variables are best used to avoid expensive local
-// object initialization each time a function is called.
-
-// * Static local variables should only be used if in your entire program and in
-// the foreseeable future of your program, the variable is unique and it
-// wouldn’t make sense to reset the variable.
-// #####################################################
+  std::cout << result << std::endl;
+  return 0;
+}
+/*
+* Use std::cout for all conventional, user-facing text.
+* For an interactive program, use std::cout for normal user-facing error
+messages (e.g. “Your input was invalid”). Use std::cerr or a logfile for status
+and diagnostic information that may be helpful for diagnosing issues but
+probably isn’t interesting for normal users. This can include technical warnings
+and errors (e.g. bad input to function x), status updates (e.g. successfully
+opened file x, failed to connect to internet service x), percentage completion
+of long tasks (e.g. encoding 50% complete), etc…
+* For a non-interactive program (tool or service), use std::cerr for error
+output only (e.g. could not open file x). This allows errors to be displayed or
+parsed separately from normal output.
+* For any application type that is transactional in nature (e.g. one that
+processes specific events, such as an interactive web browser or non-interactive
+web server), use a logfile to produce a transactional log of events that can be
+reviewed later. This can include outputting to the logfile which file is being
+processed, updates on the percentage complete, timestamps of when it started
+certain stages of computing, warning and error messages, etc…
+*/
