6fd9b7f7138ca4ddb9daf5e2191605c27c4f69e5
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-10-18 14:17:24 -0400

    Learncpp.com: section 10.4- Narrowing conversions, list initization, and

--- src/main.cpp
+++ src/main.cpp
@@ -1,193 +1,184 @@
-// Learncpp.com: section 10.3- Numeric conversions
+// Learncpp.com: section 10.4- Narrowing conversions, list initization, and
+// constexpr initializers
 
-/* // There are five basic types of numeric conversions.
-// .................................................................
-// 1) Converting an integral type to any other integral type (excluding
-// integral promotions):
-short s = 3;        // convert int to short
-long l = 3;         // convert int to long
-char ch = s;        // convert short to char
-unsigned int u = 3; // convert int to unsigned int
+/*
+n C++, a narrowing conversion is a potentially unsafe numeric conversion where
+the destination type may not be able to hold all the values of the source type.
 
-// 2) Converting a floating point type to any other floating point type
-// (excluding floating point promotions):
-float f = 3.0;        // convert double to float
-long double ld = 3.0; // convert double to long double
+The following conversions are defined to be narrowing:
 
-// 3) Converting a floating point type to any integral type:
-int i = 3.5; // convert double to int
+* From a floating point type to an integral type.
+* From a floating point type to a narrower or lesser ranked floating point type,
+unless the value being converted is constexpr and is in range of the destination
+type (even if the destination type doesn’t have the precision to store all the
+significant digits of the number).
+* From an integral to a floating point type, unless the value being converted is
+constexpr and whose value can be stored exactly in the destination type.
+* From an integral type to another integral type that cannot represent all
+values of the original type, unless the value being converted is constexpr and
+whose value can be stored exactly in the destination type. This covers both
+wider to narrower integral conversions, as well as integral sign conversions
+(signed to unsigned, or vice-versa).
 
-// 4) Converting an integral type to any floating point type:
-double d = 3; // convert int to double
+In most cases, implicit narrowing conversions will result in compiler warnings,
+with the exception of signed/unsigned conversions (which may or may not produce
+warnings, depending on how your compiler is configured).
 
-// 5) Converting an integral type or a floating point type to a bool:
-bool b1 = 3;   // convert int to bool
-bool b2 = 3.0; // convert double to bool
+Narrowing conversions should be avoided as much as possible, because they are
+potentially unsafe, and thus a source of potential errors.
 
-// NOTE: brace initialization strictly disallows some types of numeric
-conversions, we use copy initialization
- */
+BEST PRACTICE- Because they can be unsafe and are a source of errors, avoid
+narrowing conversions whenever possible.
 
-// #######################################################################################
-// An unsafe conversion is one where at least one value of the source type
-// cannot be converted into an equal value of the destination type.
-
-// Numeric conversions fall into three general safety categories:
-//..................................................................
-
-// 1) Value-preserving conversions: are safe numeric conversions where the
-// destination type can exactly represent all possible values in the source
-// type.
-
-/* int main() {
-  int n{5};
-  long l = n; // okay, produces long value 5
-
-  short s{5};
-  double d = s; // okay, produces double value 5.0
-
-  return 0;
-} */
-// Compilers will typically not issue warnings for implicit value-preserving
-// conversions.
-
-// A value converted using a value-preserving conversion can always be converted
-// back to the source type, resulting in a value that is equivalent to the
-// original value:
-/* #include <iostream>
-
-int main() {
-  int n =
-      static_cast<int>(static_cast<long>(3)); // convert int 3 to long and back
-  std::cout << n << '\n';                     // prints 3
-
-  char c = static_cast<char>(
-      static_cast<double>('c')); // convert 'c' to double and back
-  std::cout << c << '\n';        // prints 'c'
-
-  return 0;
-} */
-
-// 2) Reinterpretive conversions: are unsafe numeric conversions where the
-// converted value may be different than the source value, but no data is lost.
-// Signed/unsigned conversions fall into this category.
-// For example, when converting a signed int to an unsigned int:
-
-/* int main() {
-  int n1{5};
-  unsigned int u1{
-      n1}; // okay: will be converted to unsigned int 5 (value preserved)
-
-  int n2{-5};
-  unsigned int u2{
-      n2}; // bad: will result in large integer outside range of signed int
-
-  return 0;
-} */
-// Such value changes are typically undesirable, and will often cause the
-// program to exhibit unexpected or implementation-defined behavior.
-
-// Values converted using a reinterpretive conversion can be converted back to
-// the source type, resulting in a value equivalent to the original value (even
-// if the initial conversion produced a value out of range of the source type).
-// As such, reinterpretive conversions do not lose data during the conversion
-// process.
-
-/* #include <iostream>
-
-int main() {
-  int u = static_cast<int>(
-      static_cast<unsigned int>(-5)); // convert '-5' to unsigned and back
-  std::cout << u << '\n';             // prints -5
-
-  return 0;
-}
- */
-
-// 3) Lossy conversions: are unsafe numeric conversions where data may be lost
-// during the conversion.
-
-// For example, double to int is a conversion that may result in data loss:
-/* int i = 3.0; // okay: will be converted to int value 3 (value preserved)
-int j = 3.5; // data lost: will be converted to int value 3 (fractional value
-             // 0.5 lost) */
-// Conversion from double to float can also result in data loss:
-/* float f = 1.2; // okay: will be converted to float value 1.2 (value
-preserved)
-float g = 1.23456789; // data lost: will be converted to float 1.23457
-                      // (precision lost)
 */
 
-// Converting a value that has lost data back to the source type will result in
-// a value that is different than the original value:
+//-----------------------------------------------------------------
+// Make intentional narrowing conversions explicit
 
-/* #include <iostream>
+// Narrowing conversions are not always avoidable -- this is particularly true
+// for function calls, where the function parameter and argument may have
+// mismatched types and require a narrowing conversion.
+
+// In such cases, it is a good idea to convert an implicit narrowing conversion
+// into an explicit narrowing conversion using static_cast. Doing so helps
+// document that the narrowing conversion is intentional, and will suppress any
+// compiler warnings or errors that would otherwise result.
+
+// BEST PRACTICE- If you need to perform a narrowing conversion, use static_cast
+// to convert it into an explicit conversion.
+/*
+void someFcn(int i) {}
 
 int main() {
-  double d{static_cast<double>(
-      static_cast<int>(3.5))}; // convert double 3.5 to int and back
-  std::cout << d << '\n';      // prints 3
+  double d{5.0};
 
-  double d2{static_cast<double>(static_cast<float>(
-      1.23456789))};       // convert double 1.23456789 to float and back
-  std::cout << d2 << '\n'; // prints 1.23457
+  someFcn(
+      d); // bad: implicit narrowing conversion will generate compiler warning
+
+  // good: we're explicitly telling the compiler this narrowing conversion is
+  // intentional
+  someFcn(static_cast<int>(d)); // no warning generated
 
   return 0;
 } */
-// For example, if double value 3.5 is converted to int value 3, the fractional
-// component 0.5 is lost. When 3 is converted back to a double, the result
-// is 3.0, not 3.5.
 
-// Compilers will generally issue a warning (or in some cases, an error) when an
-// implicit lossy conversion would be performed at runtime.
-
-// Unsafe conversions should be avoided as much as possible. However, this is
-// not always possible. When unsafe conversions are used, it is most often when:
-// * We can constrain the values to be converted to just those that can be
-// converted to equal values. For example, an int can be safely converted to an
-// unsigned int when we can guarantee that the int is non-negative.
-// * We don’t mind that some data is lost because it is not relevant. For
-// example, converting an int to a bool results in the loss of data, but we’re
-// typically okay with this because we’re just checking if the int has value 0
-// or not.
-// ##########################################################
-// * In all cases, converting a value into a type whose range doesn’t support
-// that value will lead to results that are probably unexpected. For example:
-/* #include <iostream>
-int main() {
-  int i{30000};
-  char c = i; // chars have range -128 to 127
-
-  std::cout << static_cast<int>(c) << '\n';
+//----------------------------------------------------------------------
+// Narrowing conversions are disallowed when using brace initialization (which
+// is one of the primary reasons this initialization form is preferred), and
+// attempting to do so will produce a compile error.
+/* int main() {
+  int i{3.5}; // won't compile
 
   return 0;
 } */
-// In this example, we’ve assigned a large integer to a variable with type char
-// (that has range -128 to 127). This causes the char to overflow, and produces
-// an unexpected result:
+// If you actually want to do a narrowing conversion inside a brace
+// initialization, use static_cast to convert the narrowing conversion into an
+// explicit conversion:
+/* int main() {
+  double d{3.5};
 
-// * Remember that overflow is well-defined for unsigned values and produces
-// undefined behavior for signed values.
-// * Converting from a larger integral or floating point type to a smaller type
-// from the same family will generally work so long as the value fits in the
-// range of the smaller type. For example:
-/* #include <iostream>
-int main() {
-  int i{10};
-  float f = i;
-  std::cout << f << '\n';
+  // static_cast<int> converts double to int, initializes i with int result
+  int i{static_cast<int>(d)};
+
+  return 0;
 } */
 
-// * Converting from a floating point to an integer works as long as the value
-// fits within the range of the integer, but any fractional values are lost. For
-// example:
+//---------------------------------------------------------
+
+// When the source value of a narrowing conversion isn’t known until runtime,
+// the result of the conversion also can’t be determined until runtime. In such
+// cases, whether the narrowing conversion preserves the value or not also can’t
+// be determined until runtime. For example:
+/*
 #include <iostream>
-int main() {
-  int i = 3.5;
-  std::cout << i << '\n';
-  // In this example, the fractional value (.5) is lost, leaving the following
-  // result:
+
+void print(unsigned int u) // note: unsigned
+{
+  std::cout << u << '\n';
 }
-// While the numeric conversion rules might seem scary, in reality the compiler
-// will generally warn you if you try to do something dangerous (excluding some
-// signed/unsigned conversions).
+
+int main() {
+  std::cout << "Enter an integral value: ";
+  int n{};
+  std::cin >> n; // enter 5 or -5
+  print(n);      // conversion to unsigned may or may not preserve value
+
+  return 0;
+} */
+// In the above program, the compiler has no idea what value will be entered for
+// n. When print(n) is called, the conversion from int to unsigned int will be
+// performed at that time, and the results may be value-preserving or not
+// depending on what value for n was entered. Thus, a compiler that has
+// signed/unsigned warnings enabled will issue a warning for this case.
+
+// ####################################################################
+// When the source value of a narrowing conversion is constexpr, the specific
+// value to be converted must be known to the compiler. In such cases, the
+// compiler can perform the conversion itself, and then check whether the value
+// was preserved. If the value was not preserved, the compiler can halt
+// compilation with an error. If the value is preserved, the conversion is not
+// considered to be narrowing (and the compiler can replace the entire
+// conversion with the converted result, knowing that doing so is safe).
+/* #include <iostream>
+
+int main() {
+  constexpr int n1{5}; // note: constexpr
+  unsigned int u1{
+      n1}; // okay: conversion is not narrowing due to exclusion clause
+
+  constexpr int n2{-5}; // note: constexpr
+                        // unsigned int u2{n2}; // compile error: conversion is
+                        // narrowing due to value change
+
+  return 0;
+}
+ */
+
+// Strangely, conversions from a floating point type to an integral type do not
+// have a constexpr exclusion clause, so these are always considered narrowing
+// conversions even when the value to be converted is constexpr and fits in the
+// range of the destination type:
+// int n{5.0}; // compile error: narrowing conversion
+
+// Even more strangely, conversions from a constexpr floating point type to a
+// narrower floating point type are not considered narrowing even when there is
+// a loss of precision !
+// constexpr double d{0.1};
+// float f{d}; // not narrowing, even though loss of precision results
+
+// ####################################################################
+// These constexpr exception clauses are incredibly useful when list
+// initializing non-int/non-double objects, as we can use an int or double
+// literal (or a constexpr object) initialization value.
+// This allows us to avoid:
+// * Having to use literal suffixes in most cases
+// * Having to clutter our initializations with a static_cast
+/* int main() {
+  // We can avoid literals with suffixes
+  unsigned int u{5}; // okay (we don't need to use `5u`)
+  float f{1.5};      // okay (we don't need to use `1.5f`)
+
+  // We can avoid static_casts
+  constexpr int n{5};
+  double d{n}; // okay (we don't need a static_cast here)
+  short s{5}; // okay (there is no suffix for short, we don't need a static_cast
+              // here)
+
+  return 0;
+} */
+// This also works with copy and direct initialization.
+
+// One caveat worth mentioning: initializing a narrower or lesser ranked
+// floating point type with a constexpr value is allowed as long as the value is
+// in range of the destination type, even if the destination type doesn’t have
+// enough precision to precisely store the value!
+
+// Something like this is legal and will not remit an error
+int main() {
+  float f{
+      1.23456789}; // not a narrowing conversion, even though precision lost!
+
+  return 0;
+}
+// compiler may still issue a warning in this case (GCC and Clang do if you use
+// the -Wconversion compile flag).
