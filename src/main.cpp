3de4bf73b6a499ad5ec073f5a0fe2cc9c23189f3
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-10-18 11:17:50 -0400

    Learncpp.com: section 9.6- Assert and Static Assert

--- src/main.cpp
+++ src/main.cpp
@@ -1,327 +1,133 @@
-// Learncpp.com: section 9.5- std::cin and handling invalid input
+// Learncpp.com: section 9.6- Assert and Static Assert
 
-// Simplified view of how operator>> works for input:
-
-/*
-1) First, leading whitespace (spaces, tabs, and newlines at the front of the
-buffer) is discarded from the input buffer. This will discard any unextracted
-newline character remaining from a prior line of input.
-2) If the input buffer is now empty, operator>> will wait for the user to enter
-more data. Leading whitespace is again discarded.
-3) operator>> then extracts as many consecutive characters as it can, until it
-encounters either a newline character (representing the end of the line of
-input) or a character that is not valid for the variable being extracted to. The
-result of the extraction is as follows:
-
-* If any characters were extracted in step 3 above, extraction is a success. The
-extracted characters are converted into a value that is then assigned to the
-variable.
-* If no characters could be extracted in step 3 above, extraction has
-failed. The object being extracted to is assigned the value 0 (as of C++11), and
-any future extractions will immediately fail (until std::cin is cleared).
-*/
-
-// ----------------------------
-// process of checking whether user input conforms to what the program is
-// expecting is called input validation. 3 Basic Ways:
-/*
-Inline (as the user types):
----------------------------
-* Prevent the user from typing invalid input in the first place.
-
-Post-entry (after the user types):
----------------------------------
-* Let the user enter whatever they want into a string, then validate whether the
-string is correct, and if so, convert the string to the final variable format.
-* Let the user enter whatever they want, let std::cin and operator>> try to
-extract it, and handle the error cases.
-*/
-
-/*
-We can generally separate input text errors into four types:
------------------------
-* Input extraction succeeds but the input is meaningless to the program (e.g.
-entering ‘k’ as your mathematical operator).
-- The solution here is simple: do input validation. This usually consists of 3
-steps:
-
-- Check whether the user’s input was what you were expecting.
-  > If so, return the value to the caller.
-  > If not, tell the user something went wrong and have them try again.
-
-* Input extraction succeeds but the user enters additional input (e.g. entering
-‘*q hello’ as your mathematical operator).
-* Input extraction fails (e.g. trying to enter ‘q’ into a numeric input).
-* Input extraction succeeds but the user overflows a numeric value.
-*/
-
-// ###################################
-// Error case 2: Extraction succeeds but with extraneous input
-
-/* // Ignore characters
-std::cin.ignore(100, '\n'); // clear up to 100 characters out of the buffer, or
-                            // until a '\n' character is removed
-
-// To ignore everything up to and including the next ‘\n’ character, we call
-std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-#include <limits> // for std::numeric_limits
-
-void ignoreLine() {
-  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-}
-
-double getDouble() {
-  std::cout << "Enter a decimal number: ";
-  double x{};
-  std::cin >> x;
-
-  ignoreLine();
-  return x;
-}
- */
-// ###############################################
-/* // Error case 3: Extraction fails
-
-// In order to get std::cin working property again, we typically need do three
-// things:
-
-// * Detect that a prior extraction has failed.
-// * Put std::cin back in normal operation mode.
-// * Remove the input that caused the failure (so the next extraction request
-// doesn’t fail in an identical manner).
-
-if (std::cin.fail()) // If the previous extraction failed
-{
-  // Let's handle the failure
-  std::cin.clear(); // Put us back in 'normal' operation mode
-  ignoreLine();     // And remove the bad input
-}
-// Because std::cin has a Boolean conversion indicating whether the last input
-// succeeded, it’s more idiomatic to write the above as following:
-if (!std::cin) // If the previous extraction failed
-{
-  // Let's handle the failure
-  std::cin.clear(); // Put us back in 'normal' operation mode
-  ignoreLine();     // And remove the bad input
-}
-
-// Integrated into getDouble function
-double getDouble() {
-  while (true) // Loop until user enters a valid input
-  {
-    std::cout << "Enter a decimal number: ";
-    double x{};
-    std::cin >> x;
-
-    if (!std::cin) // If the previous extraction failed
-    {
-      // Let's handle the failure
-      std::cin.clear(); // Put us back in 'normal' operation mode
-      ignoreLine();     // And remove the bad input
-      continue;
-    }
-
-    // Our extraction succeeded
-    ignoreLine(); // Ignore any additional input on this line
-    return x;     // Return the value we extracted
-  }
-}
-
-// It is fine to call clear() even when extraction hasn’t failed -- it won’t do
-// anything. In cases where we are going to call ignoreLine() regardless of
-// whether we succeeded or failed, we can essentially combine the two cases:
-double getDouble() {
-  while (true) // Loop until user enters a valid input
-  {
-    std::cout << "Enter a decimal number: ";
-    double x{};
-    std::cin >> x;
-
-    bool success{std::cin}; // Remember whether we had a successful extraction
-    std::cin
-        .clear(); // Put us back in 'normal' operation mode (in case we failed)
-    ignoreLine(); // Ignore any additional input on this line (regardless)
-
-    if (success) // If we actually extracted a value
-      return x;  // Return it (otherwise, we go back to top of loop)
-  }
-}
- */
-
-// #########################################
-/* //  Checking for EOF
-
-// It is fine to call clear() even when extraction hasn’t failed -- it won’t do
-// anything. In cases where we are going to call ignoreLine() regardless of
-// whether we succeeded or failed, we can essentially combine the two cases:
-#include <limits> // for std::numeric_limits
-
-void ignoreLine()
-{
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-}
-
-// returns true if extraction failed, false otherwise
-bool clearFailedExtraction()
-{
-    // Check for failed extraction
-    if (!std::cin) // If the previous extraction failed
-    {
-        if (std::cin.eof()) // If the user entered an EOF
-        {
-            std::exit(0); // Shut down the program now
-        }
-
-        // Let's handle the failure
-        std::cin.clear(); // Put us back in 'normal' operation mode
-        ignoreLine();     // And remove the bad input
-
-        return true;
-    }
-
-    return false;
-}
-
- */
+// In a function that takes parameters, the caller may be able to pass in
+// arguments that are syntactically valid but semantically meaningless
 
 // ###############################################
-/* // Error case 4: Extraction succeeds but the user overflows a numeric value
-#include <cstdint>
+/* //  Preconditions, invariants, and postconditions
+//-----------------------------------------------
+//  * In programming, a precondition is any condition that must be true prior to
+//  the execution of some section of code (typically the body of a function). In
+//  the prior example, our check that y != 0 is a precondition that ensures y
+//  has a non-zero value before dividing by y.
+
+// Preconditions for a function are best placed at the top of a function, using
+// an early return to return back to the caller if the precondition isn’t met
+void printDivision(int x, int y) {
+  if (y == 0) // handle
+  {
+    std::cerr << "Error: Could not divide by zero\n";
+    return; // bounce the user back to the caller
+  }
+
+  // We now know that y != 0
+  std::cout << static_cast<double>(x) / y;
+}
+// * An invariant is a condition that must be true while some section of code is
+// executing. This is often used with loops, where the loop body will only
+// execute so long as the invariant is true.
+
+// * Similarly, a postcondition is something that must be true after the
+// execution of some section of code. Our function doesn’t have any
+// postconditions.
+ */
+// ##############################################
+
+/* // An assertion is an expression that will be true unless there is a bug in
+the
+// program. If the expression evaluates to true, the assertion statement does
+// nothing. If the conditional expression evaluates to false, an error message
+// is displayed and the program is terminated (via std::abort). This error
+// message typically contains the expression that failed as text, along with the
+// name of the code file and the line number of the assertion
+
+// In C++, runtime assertions are implemented via the assert preprocessor macro,
+// which lives in the <cassert> header.
+
+#include <cassert> // for assert()
+#include <cmath>   // for std::sqrt
 #include <iostream>
 
-int main()
-{
-    std::int16_t x{}; // x is 16 bits, holds from -32768 to 32767
-    std::cout << "Enter a number between -32768 and 32767: ";
-    std::cin >> x;
+double calculateTimeUntilObjectHitsGround(double initialHeight,
+                                          double gravity) {
+  assert(gravity > 0.0); // The object won't reach the ground unless there is
+                         // positive gravity.
 
-    std::int16_t y{}; // y is 16 bits, holds from -32768 to 32767
-    std::cout << "Enter another number between -32768 and 32767: ";
-    std::cin >> y;
-
-    std::cout << "The sum is: " << x + y << '\n';
-    return 0;
-} */
-/*
-What happens if the user enters a number that is too large (e.g. 40000)?
-In the above case, std::cin goes immediately into “failure mode”, but also
-assigns the closest in-range value to the variable. When the entered value is
-larger than the largest possible value for the type, the closest in-range value
-is the largest possible value for the type. Consequently, x is left with the
-assigned value of 32767. Additional inputs are skipped, leaving y with the
-initialized value of 0. We can handle this kind of error in the same way as a
-failed extraction.*/
-// #####################################
-
-// Putting it all together
-#include <cstdlib> // for std::exit
-#include <iostream>
-#include <limits> // for std::numeric_limits
-
-void ignoreLine() {
-  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-}
-
-// returns true if extraction failed, false otherwise
-bool clearFailedExtraction() {
-  // Check for failed extraction
-  if (!std::cin) // If the previous extraction failed
-  {
-    if (std::cin.eof()) // If the stream was closed
-    {
-      std::exit(0); // Shut down the program now
-    }
-
-    // Let's handle the failure
-    std::cin.clear(); // Put us back in 'normal' operation mode
-    ignoreLine();     // And remove the bad input
-
-    return true;
+  if (initialHeight <= 0.0) {
+    // The object is already on the ground. Or buried.
+    return 0.0;
   }
 
-  return false;
-}
-
-double getDouble() {
-  while (true) // Loop until user enters a valid input
-  {
-    std::cout << "Enter a decimal number: ";
-    double x{};
-    std::cin >> x;
-
-    if (clearFailedExtraction()) {
-      std::cout << "Oops, that input is invalid.  Please try again.\n";
-      continue;
-    }
-
-    ignoreLine(); // Remove any extraneous input
-    return x;     // Return the value we extracted
-  }
-}
-
-char getOperator() {
-  while (true) // Loop until user enters a valid input
-  {
-    std::cout << "Enter one of the following: +, -, *, or /: ";
-    char operation{};
-    std::cin >> operation;
-
-    if (!clearFailedExtraction()) // we'll handle error messaging if extraction
-                                  // failed below
-      ignoreLine(); // remove any extraneous input (only if extraction succeded)
-
-    // Check whether the user entered meaningful input
-    switch (operation) {
-    case '+':
-    case '-':
-    case '*':
-    case '/':
-      return operation; // Return the entered char to the caller
-    default:            // Otherwise tell the user what went wrong
-      std::cout << "Oops, that input is invalid.  Please try again.\n";
-    }
-  }
-}
-
-void printResult(double x, char operation, double y) {
-  std::cout << x << ' ' << operation << ' ' << y << " is ";
-
-  switch (operation) {
-  case '+':
-    std::cout << x + y << '\n';
-    return;
-  case '-':
-    std::cout << x - y << '\n';
-    return;
-  case '*':
-    std::cout << x * y << '\n';
-    return;
-  case '/':
-    if (y == 0.0)
-      break;
-
-    std::cout << x / y << '\n';
-    return;
-  }
-
-  std::cout << "???"; // Being robust means handling unexpected parameters as
-                      // well, even though getOperator() guarantees operation is
-                      // valid in this particular program
+  return std::sqrt((2.0 * initialHeight) / gravity);
 }
 
 int main() {
-  double x{getDouble()};
-  char operation{getOperator()};
-  double y{getDouble()};
-
-  // Handle division by 0
-  while (operation == '/' && y == 0.0) {
-    std::cout << "The denominator cannot be zero.  Try again.\n";
-    y = getDouble();
-  }
-
-  printResult(x, operation, y);
+  std::cout << "Took " << calculateTimeUntilObjectHitsGround(100.0, -9.8)
+            << " second(s)\n";
 
   return 0;
-}
+} */
+
+// Make assert statements more descriptive(string literal always evaluates to
+// Boolean true): assert(found && "Car could not be found in database");
+
+// Can use assertions to document cases that were not implemented because not
+// needed at the time. code will fail with a useful error message, and the
+// programmer can then determine how to implement that case
+// assert(moved && "Need to handle case where student was just moved to another
+// classroom");
+
+// ##################################################
+// NDEBUG
+// assert macro comes with a small performance cost that is incurred each time
+// the assert condition is checked. Furthermore, asserts should (ideally) never
+// be encountered in production code (because your code should already be
+// thoroughly tested). Consequently, most developers prefer that asserts are
+// only active in debug builds. C++ comes with a built-in way to turn off
+// asserts in production code: if the preprocessor macro NDEBUG is defined, the
+// assert macro gets disabled.
+
+// ######################################################
+//  static_assert
+
+// A static_assert is an assertion that is checked at compile-time rather than
+// at runtime, with a failing static_assert causing a compile error. Unlike
+// assert, which is declared in the <cassert> header, static_assert is a
+// keyword, so no header needs to be included to use it.
+
+static_assert(sizeof(long) == 8, "long must be 8 bytes");
+static_assert(sizeof(int) >= 4, "int must be at least 4 bytes");
+
+int main() { return 0; }
+// Useful notes on static_assert
+// ------------------------------------------
+// * Because static_assert is evaluated by the compiler, the condition must be a
+// constant expression.
+// * static_assert can be placed anywhere in the code file (even in the global
+// namespace).
+// * static_assert is not deactivated in release builds (like normal assert is).
+// * Because the compiler does the evaluation, there is no runtime cost to a
+// static_assert
+
+// BEST PRACTICE- Favor static_assert over assert() whenever possible.
+// #############################################################
+
+// Assertions are used to detect programming errors during development by
+// documenting assumptions about things that should never happen. And if they do
+// happen, it’s the fault of the programmer. Assertions do not allow recovery
+// from errors (after all, if something should never happen, there’s no need to
+// recover from it). Because asserts are typically compiled-out in release
+// builds, you can put a lot of them in without worrying about performance, so
+// there’s little reason not to use them liberally.
+
+// Error handling is used when we need to gracefully handle cases that could
+// happen (however rarely) in a release build. These may either be recoverable
+// issues (where the program can keep running), or unrecoverable issues (where
+// the program has to shut down, but we can at least show a nice error message
+// and ensure everything is cleaned up properly). Error detection and handling
+// has both a runtime performance cost and a development time cost.
+
+// *** Also note that the abort() function terminates the program immediately,
+// without a chance to do any further cleanup (e.g. close a file or database).
+// Because of this, asserts should be used only in cases where corruption isn’t
+// likely to occur if the program terminates unexpectedly.
