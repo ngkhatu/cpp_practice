6d93e3876a28a5f636cc32586ee4f2067a0e13b4
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-10-18 12:59:28 -0400

    Learncpp.com: section 10.2- Floating-point and integral promotion

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,102 @@
-// Learncpp.com: section 10.1- Implicit Type conversion
+// Learncpp.com: section 10.2- Floating-point and integral promotion
 
+// Because C++ is designed to be portable and performant across a wide range of
+// architectures, the language designers did not want to assume a given CPU
+// would be able to efficiently manipulate values that were narrower than the
+// natural data size for that CPU.
+
+// To help address this challenge, C++ defines a category of type conversions
+// informally called the numeric promotions. A numeric promotion is the type
+// conversion of certain narrower numeric types (such as a char) to certain
+// wider numeric types (typically int or double) that can be processed
+// efficiently.
+
+// All numeric promotions are value-preserving. A value-preserving conversion
+// (also called a safe conversion) is one where every possible source value can
+// be converted into an equal value of the destination type.
+
+// Because promotions are safe, the compiler will freely use numeric promotion
+// as needed, and will not issue a warning when doing so.
+
+// ----------------------------------------------------------
+// Reduce Redundancy
+// we can write functions that have int and/or double parameters (such as the
+// printInt() function). That same code can then be called with arguments
+// of types that can be numerically promoted to match the types of the function
+// parameters
+
+/* #include <iostream>
+
+void printInt(int x) { std::cout << x << '\n'; } */
+
+// ----------------------------------------------------------
+// Categories:
+// numeric promotion rules are divided into two subcategories: integral
+// promotions and floating point promotions. Only the conversions listed in
+// these categories are considered to be numeric promotions.
+// ----------------------------------------------------------
+// Floating Point Promotions
+// Using the floating point promotion rules, a value of type float can be
+// converted to a value of type double.
+
+// This means we can write a function that takes a double and then call it with
+// either a double or a float value:
+
+/* #include <iostream>
+
+void printDouble(double d) { std::cout << d << '\n'; }
+
+int main() {
+  printDouble(5.0);  // no conversion necessary
+  printDouble(4.0f); // numeric promotion of float to double
+  return 0;
+} */
+// ----------------------------------------------------------
+// Integral Promotions
 /*
-When a type conversion is invoked (whether implicitly or explicitly), the
-compiler will determine whether it can convert the value from the current type
-to the desired type. If a valid conversion can be found, then the compiler will
-produce a new value of the desired type. Note that type conversions don’t change
-the value or type of the value or object being converted.
-
-If the compiler can’t find an acceptable conversion, then the compilation will
-fail with a compile error. Type conversions can fail for any number of reasons.
-For example, the compiler might not know how to convert a value between the
-original type and the desired type. In other cases, statements may disallow
-certain types of conversions
+the following conversions can be made:
+....................................................
+* signed char or signed short can be converted to int.
+* unsigned char, char8_t, and unsigned short can be converted to int if int can
+hold the entire range of the type, or unsigned int otherwise.
+* If char is signed by default, it follows the signed char conversion rules
+above. If it is unsigned by default, it follows the unsigned char conversion
+rules above.
+* bool can be converted to int, with false becoming 0 and true becoming 1.
+Assuming an 8 bit byte and an int size of 4 bytes or larger (which is typical
+these days), the above basically means that bool, char, signed char, unsigned
+char, signed short, and unsigned short all get promoted to int.
 */
-// int x { 3.5 }; // brace-initialization disallows conversions that result in
-// data loss
+// *  while integral promotion is value-preserving, it does not necessarily
+// preserve the signedness (signed/unsigned) of the type.
+#include <iostream>
 
-/*
-The standard conversions can be broadly divided into 4 categories, each covering
-different types of conversions:
+void printInt(int x) { std::cout << x << '\n'; }
 
-- Numeric promotions (covered in lesson 10.2 -- Floating-point and integral
-promotion)
-- Numeric conversions (covered in lesson 10.3 -- Numeric conversions)
-- Arithmetic conversions (covered in lesson 10.5 -- Arithmetic conversions)
-- Other conversions (which includes various pointer and reference conversions)
+int main() {
+  printInt(2);
 
-When a type conversion is needed, the compiler will see if there are standard
-conversions that it can use to convert the value to the desired type. The
-compiler may apply zero, one, or (in certain cases) two standard conversions in
-the conversion process.
+  short s{3};  // there is no short literal suffix, so we'll use a variable for
+               // this one
+  printInt(s); // numeric promotion of short to int
 
-*/
+  printInt('a');  // numeric promotion of char to int
+  printInt(true); // numeric promotion of bool to int
+
+  return 0;
+}
+// ----------------------------------------------------------
+// Not all widening converstions are numberic promotions
+// widening type conversions (such as char to short, or int to long) are not
+// considered to be numeric promotions in C++ (they are numeric conversions,
+// which we’ll cover shortly in lesson 10.3 -- Numeric conversions). This is
+// because such conversions do not assist in the goal of converting smaller
+// types to larger types that can be processed more efficiently.
+
+// ----------------------------------------------------------
+// ----------------------------------------------------------
+// ----------------------------------------------------------
+// ----------------------------------------------------------
+// ----------------------------------------------------------
+// ----------------------------------------------------------
+// ----------------------------------------------------------
