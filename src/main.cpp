e2e4c1e08536a03d682ccc29fe49fbc7ea3914d4
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-10-22 11:18:17 -0400

    Learncpp.com: section 11.4- Deleting functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,42 @@
-// Learncpp.com: section 11.1- Intro to Function Overloading
+// Learncpp.com: section 11.4- Deleting functions
+/*
+#include <iostream>
+
+void printInt(int x) { std::cout << x << '\n'; }
+
+void printInt(char) = delete; // calls to this function will halt compilation
+void printInt(bool) = delete; // calls to this function will halt compilation
+
+int main() {
+  printInt(97); // okay
+
+  // printInt('a');  // compile error: function deleted
+  // printInt(true); // compile error: function deleted
+
+  // printInt(5.0); // compile error: ambiguous match
+
+  return 0;
+}
+ */
+
+// #################################################
+
+// Deleting all non-matching overloads
+//.......................................
 
 #include <iostream>
 
-int add(int x, int y) { return x + y; }
+// This function will take precedence for arguments of type int
+void printInt(int x) { std::cout << x << '\n'; }
 
-double add(double x, double y) { return x + y; }
+// This function template will take precedence for arguments of other types
+// Since this function template is deleted, calls to it will halt compilation
+template <typename T> void printInt(T x) = delete;
 
 int main() {
-  std::cout << add(1, 2); // calls add(int, int)
-  std::cout << '\n';
-  std::cout << add(1.2, 3.4); // calls add(double, double)
+  printInt(97); // okay
+  // printInt('a');  // compile error
+  // printInt(true); // compile error
 
   return 0;
 }
