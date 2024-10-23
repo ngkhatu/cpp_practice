b44ae79aa350c2cef6501538d8229e24f11a70b8
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-10-23 15:07:16 -0400

    Added md for Section 15.9- Friend classes and friend member functions. Added TOC to other md files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,129 +1,92 @@
-// Learncpp.com: Section 15.8- Friend non-member functions
+// Learncpp.com: Section 15.9- Friend classes and friend member functions
 
 /* #include <iostream>
 
-class Value {
+class Storage {
 private:
-  int m_value{};
+  int m_nValue{};
+  double m_dValue{};
 
 public:
-  explicit Value(int v) : m_value{v} {}
+  Storage(int nValue, double dValue) : m_nValue{nValue}, m_dValue{dValue} {}
 
-  bool isEqualToMember(const Value &v) const;
-  friend bool isEqualToNonmember(const Value &v1, const Value &v2);
+  // Make the Display class a friend of Storage
+  friend class Display;
 };
 
-bool Value::isEqualToMember(const Value &v) const {
-  return m_value == v.m_value;
-}
+class Display {
+private:
+  bool m_displayIntFirst{};
 
-bool isEqualToNonmember(const Value &v1, const Value &v2) {
-  return v1.m_value == v2.m_value;
-}
+public:
+  Display(bool displayIntFirst) : m_displayIntFirst{displayIntFirst} {}
+
+  // Because Display is a friend of Storage, Display members can access the
+  // private members of Storage
+  void displayStorage(const Storage &storage) {
+    if (m_displayIntFirst)
+      std::cout << storage.m_nValue << ' ' << storage.m_dValue << '\n';
+    else // display double first
+      std::cout << storage.m_dValue << ' ' << storage.m_nValue << '\n';
+  }
+
+  void setDisplayIntFirst(bool b) { m_displayIntFirst = b; }
+};
 
 int main() {
-  Value v1{5};
-  Value v2{6};
+  Storage storage{5, 6.7};
+  Display display{false};
 
-  std::cout << v1.isEqualToMember(v2) << '\n';
-  std::cout << isEqualToNonmember(v1, v2) << '\n';
+  display.displayStorage(storage);
+
+  display.setDisplayIntFirst(true);
+  display.displayStorage(storage);
 
   return 0;
 }
  */
 
-// ##############################################
+// #################################################################
 
-/* #include <iostream>
-
-class Humidity; // forward declaration of Humidity
-
-class Temperature {
-private:
-  int m_temp{0};
-
-public:
-  explicit Temperature(int temp) : m_temp{temp} {}
-
-  friend void printWeather(
-      const Temperature &temperature,
-      const Humidity &humidity); // forward declaration needed for this line
-};
-
-class Humidity {
-private:
-  int m_humidity{0};
-
-public:
-  explicit Humidity(int humidity) : m_humidity{humidity} {}
-
-  friend void printWeather(const Temperature &temperature,
-                           const Humidity &humidity);
-};
-
-void printWeather(const Temperature &temperature, const Humidity &humidity) {
-  std::cout << "The temperature is " << temperature.m_temp
-            << " and the humidity is " << humidity.m_humidity << '\n';
-}
-
-int main() {
-  Humidity hum{10};
-  Temperature temp{12};
-
-  printWeather(temp, hum);
-
-  return 0;
-}
- */
-// #############################################################
-/*
 #include <iostream>
+class Storage;
 
-class Accumulator {
+class Display {
 private:
-  int m_value{0}; // if we rename this
+  bool m_displayIntFirst{};
 
 public:
-  void add(int value) { m_value += value; } // we need to modify this
+  Display(bool displayIntFirst) : m_displayIntFirst{displayIntFirst} {}
 
-  friend void print(const Accumulator &accumulator);
+  void displayStorage(const Storage &storage);
 };
 
-void print(const Accumulator &accumulator) {
-  std::cout << accumulator.m_value; // and we need to modify this
+class Storage {
+private:
+  int m_nValue{};
+  double m_dValue{};
+
+public:
+  Storage(int nValue, double dValue) : m_nValue{nValue}, m_dValue{dValue} {}
+
+  // Make the Display::displayStorage member function a friend of the Storage
+  // class
+  friend void Display::displayStorage(
+      const Storage &storage); // error: Storage hasn't seen the full definition
+                               // of class Display
+};
+
+void Display::displayStorage(const Storage &storage) {
+  if (m_displayIntFirst)
+    std::cout << storage.m_nValue << ' ' << storage.m_dValue << '\n';
+  else // display double first
+    std::cout << storage.m_dValue << ' ' << storage.m_nValue << '\n';
 }
 
 int main() {
-  Accumulator acc{};
-  acc.add(5); // add 5 to the accumulator
-
-  print(acc); // call the print() non-member function
-
-  return 0;
-} */
-// ---------------------------------------------------------
-#include <iostream>
-
-class Accumulator {
-private:
-  int m_value{0};
-
-public:
-  void add(int value) { m_value += value; }
-  int value() const { return m_value; } // added this reasonable access function
-};
-
-void print(const Accumulator &accumulator) // no longer a friend of Accumulator
-{
-  std::cout
-      << accumulator.value(); // use access function instead of direct access
-}
-
-int main() {
-  Accumulator acc{};
-  acc.add(5); // add 5 to the accumulator
-
-  print(acc); // call the print() non-member function
+  Storage storage{5, 6.7};
+  Display display{false};
+  display.displayStorage(storage);
 
   return 0;
 }
