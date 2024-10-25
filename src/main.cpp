7e1c8262326083f587d7491e09ad8450523e80c0
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-10-25 15:41:35 -0400

    16.9- Array indexing and length using enumerators

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
-#include <ranges> // C++20
-#include <string_view>
+#include <type_traits> // for std::underlying_type_t
 #include <vector>
 
+enum class StudentNames {
+  kenny,       // 0
+  kyle,        // 1
+  stan,        // 2
+  butters,     // 3
+  cartman,     // 4
+  max_students // 5
+};
+
+// Overload the unary + operator to convert StudentNames to the underlying type
+constexpr auto operator+(StudentNames a) noexcept {
+  return static_cast<std::underlying_type_t<StudentNames>>(a);
+}
+
 int main() {
-  std::vector<std::string_view> words{"Alex", "Bobby", "Chad",
-                                      "Dave"}; // sorted in alphabetical order
+  std::vector<int> testScores(+StudentNames::max_students);
 
-  for (const auto &word : std::views::reverse(words)) // create a reverse view
-    std::cout << word << ' ';
+  testScores[+StudentNames::stan] = 76;
 
-  std::cout << '\n';
+  std::cout << "The class has " << +StudentNames::max_students << " students\n";
 
   return 0;
 }
