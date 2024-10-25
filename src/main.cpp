8d5c967f0cb1a3d74b540b894ddefa2153a5ff63
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-10-25 13:23:59 -0400

    16.8- Range-based for loops

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,16 @@
-/* #include "SignedArrayView.h"
 #include <iostream>
+#include <ranges> // C++20
+#include <string_view>
 #include <vector>
 
 int main() {
-  std::vector arr{9, 7, 5, 3, 1};
-  SignedArrayView sarr{arr}; // Create a signed view of our std::vector
+  std::vector<std::string_view> words{"Alex", "Bobby", "Chad",
+                                      "Dave"}; // sorted in alphabetical order
 
-  for (auto index{sarr.ssize() - 1}; index >= 0; --index)
-    std::cout << sarr[index] << ' '; // index using a signed type
+  for (const auto &word : std::views::reverse(words)) // create a reverse view
+    std::cout << word << ' ';
 
-  return 0;
-}
- */
-
-// ######################################
-
-#include <cstddef>
-#include <iostream>
-#include <vector>
-
-using Index = std::ptrdiff_t;
-
-int main() {
-  std::vector arr{9, 7, 5, 3, 1};
-
-  auto length{static_cast<Index>(arr.size())}; // in C++20, prefer std::ssize()
-  for (auto index{length - 1}; index >= 0; --index)
-    std::cout << arr.data()[index]
-              << ' '; // use data() to avoid sign conversion warning
+  std::cout << '\n';
 
   return 0;
 }
