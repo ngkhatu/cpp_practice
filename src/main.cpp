1add6b07c62823c28adb418158165073085e31ce
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-10-26 14:08:48 -0400

    17.2_std::array length and indexing

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,61 @@
+/* #include <array>
+#include <iostream>
+
+int main() {
+  std::array arr{9.0, 7.2, 5.4, 3.6, 1.8};
+  std::cout
+      << "length: " << arr.size()
+      << '\n'; // returns length as type `size_type` (alias for `std::size_t`)
+
+  std::cout << "length: "
+            << std::size(arr); // C++17, returns length as type `size_type`
+                               // (alias for `std::size_t`)
+
+  std::cout
+      << "length: "
+      << std::ssize(
+             arr); // C++20, returns length as a large signed integral type
+
+  constexpr int length{
+      std::size(arr)}; // ok: return value is constexpr std::size_t and can be
+                       // converted to int, not a narrowing conversion
+  std::cout << "length: " << length << '\n';
+
+  return 0;
+}
+ */
+
+// #############################################################
+
+#include <__atomic/aliases.h>
 #include <array>
 #include <cstddef>
 #include <iostream>
 
+/* void printLength(const std::array<int, 5> &arr)
+{
+    constexpr int length{ std::size(arr) }; // compile error!
+    std::cout << "length: " << length << '\n';
+} */
+
+// workaround
+template <auto Length> void printLength(const std::array<int, Length> &arr) {
+  // constexpr int length{std::size(arr)}; // compile error!
+  std::cout << "length: " << Length << '\n';
+}
+
 int main() {
+  constexpr std::array arr{9, 7, 5, 3, 1};
+  constexpr int length{std::size(arr)}; // works just fine
 
-  const int numStudents{6};
+  constexpr int val{arr[3]};
 
-  constexpr std::array<int, numStudents> arr{0, 1, 2};
-  constexpr std::array a1{9, 7, 5, 3,
-                          1}; // The type is deduced to std::array<int, 5>
-  constexpr std::array a2{9.7,
-                          7.31}; // The type is deduced to std::array<double, 2>
-  // constexpr std::array<int> a3{9, 7, 5, 3, 1}; // error: too few template
-  // arguments (length missing)
-  // constexpr std::array<5> a4{9, 7, 5, 3, 1}; // error: too few template
-  // arguments (type missing)
+  std::cout << "length: " << length << '\n';
 
-  constexpr auto myArray1{
-      std::to_array<int, 5>({9, 7, 5, 3, 1})}; // Specify type and size
-  constexpr auto myArray2{
-      std::to_array<int>({9, 7, 5, 3, 1})}; // Specify type only, deduce size
-  constexpr auto myArray3{
-      std::to_array({9, 7, 5, 3, 1})}; // Deduce type and size
+  printLength(arr);
 
-  if (!arr.empty())
-    std::cout << "NOT EMPTY!" << std::endl;
-  for (auto i : arr) {
-    std::cout << i << '\t';
-  }
-  std::cout << '\n';
-
-  constexpr auto shortArray{std::to_array<short>({9, 7, 5, 3, 1})};
-  std::cout << sizeof(shortArray[0]) << '\n';
-
-  constexpr std::array<int, 5> prime{2, 3, 5, 7, 11};
-
-  std::cout << prime[3]; // print the value of element with index 3 (7)
-  std::cout << prime[9]; // invalid index (undefined behavior)
+  std::cout << std::get<3>(arr); // print the value of element with index 3
+  std::cout << std::get<2>(arr); // invalid index (compile error)
 
   return 0;
 }
