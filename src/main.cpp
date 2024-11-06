ae373690a8d562f67931fd3f9b74718c1775b16e
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-11-06 12:59:45 -0500

    Learncpp.com- Chapter 19

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,45 @@
+#include <cassert>
 #include <iostream>
 
+enum class Type {
+  tInt, // note: we can't use "int" here because it's a keyword, so we'll use
+        // "tInt" instead
+  tFloat,
+  tCString
+};
+
+void printValue(void *ptr, Type type) {
+  switch (type) {
+  case Type::tInt:
+    std::cout << *static_cast<int *>(ptr)
+              << '\n'; // cast to int pointer and perform indirection
+    break;
+  case Type::tFloat:
+    std::cout << *static_cast<float *>(ptr)
+              << '\n'; // cast to float pointer and perform indirection
+    break;
+  case Type::tCString:
+    std::cout << static_cast<char *>(ptr)
+              << '\n'; // cast to char pointer (no indirection)
+    // std::cout will treat char* as a C-style string
+    // if we were to perform indirection through the result, then we'd just
+    // print the single char that ptr is pointing to
+    break;
+  default:
+    std::cerr << "printValue(): invalid type provided\n";
+    assert(false && "type not found");
+    break;
+  }
+}
+
 int main() {
+  int nValue{5};
+  float fValue{7.5f};
+  char szValue[]{"Mollie"};
 
-  int *arr{new int[1000000]{}};
-
-  arr[5000] = 50;
-
-  std::cout << arr[5000] << std::endl;
+  printValue(&nValue, Type::tInt);
+  printValue(&fValue, Type::tFloat);
+  printValue(szValue, Type::tCString);
 
   return 0;
 }
