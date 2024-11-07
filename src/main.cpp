c860109ece151e573b7fb8caaae1f27160566b75
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-11-06 23:32:44 -0500

    20.5

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,36 @@
-// Program: MyArgs
+#include <cstdarg> // needed to use ellipsis
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-  std::cout << "There are " << argc << " arguments:\n";
+// The ellipsis must be the last parameter
+// count is how many additional arguments we're passing
+double findAverage(int count, ...) {
+  int sum{0};
 
-  // Loop through each argument and print its number and value
-  for (int count{0}; count < argc; ++count) {
-    std::cout << count << ' ' << argv[count] << '\n';
+  // We access the ellipsis through a va_list, so let's declare one
+  std::va_list list;
+
+  // We initialize the va_list using va_start.  The first argument is
+  // the list to initialize.  The second argument is the last non-ellipsis
+  // parameter.
+  va_start(list, count);
+
+  // Loop through all the ellipsis values
+  for (int arg{0}; arg < count; ++arg) {
+    // We use va_arg to get values out of our ellipsis
+    // The first argument is the va_list we're using
+    // The second argument is the type of the value
+    sum += va_arg(list, double);
   }
 
+  // Cleanup the va_list when we're done.
+  va_end(list);
+
+  return static_cast<double>(sum) / count;
+}
+
+int main() {
+  std::cout << findAverage(5, 1, 2, 3, 4, 5) << '\n';
+  std::cout << findAverage(6, 1, 2, 3, 4, 5, 6) << '\n';
+  std::cout << findAverage(6, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0) << '\n';
   return 0;
 }
