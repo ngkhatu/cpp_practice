62668999c6f941e8eea27211998a52880dfa8c36
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-11-07 19:49:12 -0500

    21.3

---
+++ src/Cents.h
@@ -0,0 +1,18 @@
+#ifndef CENTS_H
+#define CENTS_H
+
+class Cents {
+private:
+  int m_cents{};
+
+public:
+  Cents(int cents) : m_cents{cents} {}
+
+  int getCents() const { return m_cents; }
+};
+
+// Need to explicitly provide prototype for operator+ so uses of operator+ in
+// other files know this overload exists
+Cents operator+(const Cents &c1, const Cents &c2);
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,13 @@
-#include <cstdarg> // needed to use ellipsis
+#include "Cents.h"
 #include <iostream>
 
-// The ellipsis must be the last parameter
-// count is how many additional arguments we're passing
-double findAverage(int count, ...) {
-  int sum{0};
-
-  // We access the ellipsis through a va_list, so let's declare one
-  std::va_list list;
-
-  // We initialize the va_list using va_start.  The first argument is
-  // the list to initialize.  The second argument is the last non-ellipsis
-  // parameter.
-  va_start(list, count);
-
-  // Loop through all the ellipsis values
-  for (int arg{0}; arg < count; ++arg) {
-    // We use va_arg to get values out of our ellipsis
-    // The first argument is the va_list we're using
-    // The second argument is the type of the value
-    sum += va_arg(list, double);
-  }
-
-  // Cleanup the va_list when we're done.
-  va_end(list);
-
-  return static_cast<double>(sum) / count;
-}
-
 int main() {
-  std::cout << findAverage(5, 1, 2, 3, 4, 5) << '\n';
-  std::cout << findAverage(6, 1, 2, 3, 4, 5, 6) << '\n';
-  std::cout << findAverage(6, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0) << '\n';
+  Cents cents1{6};
+  Cents cents2{8};
+  Cents centsSum{
+      cents1 +
+      cents2}; // without the prototype in Cents.h, this would fail to compile
+  std::cout << "I have " << centsSum.getCents() << " cents.\n";
+
   return 0;
 }
